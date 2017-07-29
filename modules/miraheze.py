4a4ca3d076c83e23c1fa6d52a2e87abf58670cc8
Reception123 <utilizator.receptie123@gmail.com>
2017-07-29 19:07:08 +0200

    correct miraheze url

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -8,4 +8,4 @@ from sopel.module import rule, priority, thread, commands
 
 @commands('miraheze')
 def miraheze(bot, trigger):
-bot.say(' ' + trigger.nick + 'Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, www.meta.miraheze.org/ or #miraheze.')
+bot.say(' ' + trigger.nick + 'Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, https://meta.miraheze.org/ or #miraheze.')
