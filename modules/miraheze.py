5a2db00e27117ebe51a93dd236dc4a471f4d0f7e
Devin/Zppix <megadev44s.mail@gmail.com>
2017-07-29 11:52:15 -0500

    Create miraheze.py

---
+++ modules/miraheze.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+from sopel.module import rule, priority, thread, commands
+
+
+@commands('miraheze')
+def miraheze(bot, trigger):
+bot.say(trigger.nick +'Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, www.meta.miraheze.org/ or #miraheze.')
