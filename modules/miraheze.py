eb0bea82ffe235954f22334a23b2ed91c51f76b6
Devin/Zppix <megadev44s.mail@gmail.com>
2017-07-29 12:01:35 -0500

    Update miraheze.py

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -5,4 +5,4 @@ from sopel.module import rule, priority, thread, commands
 
 @commands('miraheze')
 def miraheze(bot, trigger):
-bot.say(trigger.nick +'Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, www.meta.miraheze.org/ or #miraheze.')
+bot.say(' ' + trigger.nick + 'Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, www.meta.miraheze.org/ or #miraheze.')
