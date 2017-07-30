dca770f867c04c05a4bcff57a0256507ec55412e
zppix1 <Megadev44s.mail@gmail.com>
2017-07-30 16:10:41 -0500

    indent

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -13,6 +13,6 @@ This command will tell you about Miraheze and where to learn more
 @commands('miraheze')
 def miraheze(bot, trigger):
 	if trigger.sender == '#miraheze':
-    bot.say(trigger.nick + ', Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, https://meta.miraheze.org/ or ask in this channel.')
+		bot.say(trigger.nick + ', Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, https://meta.miraheze.org/ or ask in this channel.')
 	else:
-	bot.say(trigger.nick + ', Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, https://meta.miraheze.org/ or #miraheze.')
+		bot.say(trigger.nick + ', Miraheze is a non-profit wikifarm running MediaWiki. If you would like more information please see, https://meta.miraheze.org/ or #miraheze.')
