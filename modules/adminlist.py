5be3af7c3902837cced092d08e0d4d976b3cce80
Devin/Zppix <megadev44s.mail@gmail.com>
2017-07-30 11:03:43 -0500

    Add an admin list command

---
+++ modules/adminlist.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel
+import sopel.module
+import requests
+import sopel.tools
+from sopel.module import rule, priority, thread, commands
+
+@commands('botadmins','admins')
+def adminlist(bot, trigger):
+  bot.say(trigger.nick + ', the bot's admins are: Reception123, SwisterTwister, and Zppix.')
