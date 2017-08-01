bd0fd3e9ccf39827e6a03496263b197332368ee0
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-01 13:22:27 -0500

    add accesslevel cmd

--- modules/adminlist.py
+++ modules/adminlist.py
@@ -15,3 +15,14 @@ def adminlist(bot, trigger):
     bot.say(trigger.nick + ', the bot\'s admins are: Reception123, You and Zppix.')
   else:
     bot.say(trigger.nick + ', the bot\'s admins are: Reception123, SwisterTwister and Zppix.')
+
+ @commands('accesslevel')
+def accesslevel(bot,trigger):
+  if trigger.nick  == 'Reception123':
+    bot.say('The access level for' + trigger.nick + ', is Admin.')
+    elif trigger.nick == 'Zppix':
+      bot.say('The access level for' + trigger.nick + ', is Owner.')
+      elif trigger.nick == 'SwisterTwister':
+        bot.say('The access level for' + trigger.nick + ', is Admin.')
+        else:
+          bot.say('The access level for' + trigger.nick + ', is User.')
