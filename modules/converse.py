6bb631234c305867e9b918af3d870bb455cb6e9c
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-02 11:31:43 -0500

    Update converse.py

--- modules/converse.py
+++ modules/converse.py
@@ -2,7 +2,9 @@ from sopel.module import rule
 
 @rule('.*$nickname.*')
 def ping_converse(bot, trigger):
-    if trigger.nick == 'Reception123' or trigger.nick == 'Reception|away':
+    if trigger.nick == 'Reception123':
+        bot.say("I get it, Reception123, talking to real people is too overrated." % trigger.nick)
+    elif trigger.nick == 'Reception|away':
         bot.say("I get it, Reception123, talking to real people is too overrated." % trigger.nick)
     elif trigger.nick == 'PuppyKun':
         bot.say("Hit the tab button too soon again, PuppyKun? Tsk tsk tsk." % trigger.nick)
