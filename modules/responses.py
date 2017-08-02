816e1d044dd56943391af0083e746a1859b01c7f
Reception123 <utilizator.receptie123@gmail.com>
2017-08-02 19:58:17 +0200

    try adding responses.py

---
+++ modules/responses.py
@@ -0,0 +1,6 @@
+from sopel.module import rule
+
+@rule('update.php')
+def ping_converse(bot, trigger):
+        bot.say("Oh no, update.php! If you're not upgrading MediaWiki, you should really not be using that!")
+ 
