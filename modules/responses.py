77d19d4b646cde0121c3c5d7b143fdf129d6f51f
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-02 13:52:07 -0500

    Add if statement

--- modules/responses.py
+++ modules/responses.py
@@ -2,5 +2,6 @@ from sopel.module import rule
 
 @rule('update.php')
 def ping_converse(bot, trigger):
-        bot.say("Oh no, update.php! If you're not upgrading MediaWiki, you should really not be using that!")
+        if trigger.sender == '#miraheze':
+                bot.say("Oh no, update.php! If you're not upgrading MediaWiki, you should really not be using that!")
  
