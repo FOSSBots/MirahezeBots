58e91acb8c8a05fd35d680accd58542a22316fee
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-02 14:39:21 -0500

    fix cmd doc

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -25,7 +25,7 @@ def mirahezegehelp(bot, trigger):
 	bot.say(trigger.nick + ', needs help. Pinging Reception123, Zppix, PuppyKun, Voidwalker.')
 @commands('dns', 'ns', 'nameservers')
 def mhdns(bot, trigger):
-"""
+	"""
 Information about how Miraheze handles DNS/Nameservers
-"""
+	"""
 	bot.say(trigger.nick + ': Miraheze, once gaven any and all info applicable to getting your domain setup (including DNS and/or other nameservers), it will take up to 48 hours. However, this process will only start once the DNS patch has been merged. For more information, please feel free to ask')
