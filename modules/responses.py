bea61e890fab8a5573481603ba93fcbf53e36da4
Reception123 <utilizator.receptie123@gmail.com>
2017-08-03 20:15:13 +0200

    add down command (#15)
    * add down command
    
    * Update responses.py

--- modules/responses.py
+++ modules/responses.py
@@ -4,4 +4,8 @@ from sopel.module import rule
 def ping_converse(bot, trigger):
         if trigger.sender == '#miraheze':
                 bot.say("Oh no, update.php! If you're not upgrading MediaWiki, you should really not be using that!")
+@rule('my wiki is down', 'Miraheze is down')
+def ping_converse(bot, trigger):
+        if trigger.sender == '#miraheze':
+                bot.say("Oh no! That sounds bad. A sysadmin should be here shortly to investigate. If you haven't already, please file a Phabricator ticket to facilitate the process!")                
  
