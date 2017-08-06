e0f47fb2d5c1aec1405f13ed60fa935911b4c09f
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-06 10:49:52 -0500

    Update responses.py

--- modules/responses.py
+++ modules/responses.py
@@ -13,6 +13,6 @@ def ping_converse3(bot, trigger):
         if trigger.sender == '#miraheze':
                 bot.say("That sounds bad! A sysadmin should be here shortly to investigate. If you haven't already, please file a Phabricator ticket to facilitate the process!")
 @rule('The upgrade was successful')
-def ping_converse3(bot, trigger):
+def ping_converse4(bot, trigger):
         if trigger.sender == '#miraheze':
                 bot.say("Good job %s! Keep on doing what you do.")
