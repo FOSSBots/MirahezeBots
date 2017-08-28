905076af681c06adb6e2deb860998c01af2829a7
Devin/Zppix <megadev44s.mail@gmail.com>
2017-08-28 13:48:30 -0500

    Update adminlist.py

--- modules/adminlist.py
+++ modules/adminlist.py
@@ -9,13 +9,13 @@ def adminlist(bot, trigger):
    """
   Lists the current bot admins
    """
-   if trigger.nick == 'Reception123' or trigger.nick == 'Reception|away':
+if trigger.nick == 'Reception123' or trigger.nick == 'Reception|away':
     bot.say(trigger.nick + ', the bot\'s admins are: You, SwisterTwister and Zppix.')
-   elif trigger.nick == 'Zppix':
+elif trigger.nick == 'Zppix':
     bot.say(trigger.nick + ', the bot\'s admins are: Reception123, SwisterTwister and you.')
-   elif trigger.nick == 'SwisterTwister' or trigger.nick == 'StormyWaves':
-    bot.say(trigger.nick + ', the bot\'s admins are: Reception123, You and Zppix.')
-   else:
+elif trigger.nick == 'SwisterTwister' or trigger.nick == 'StormyWaves':
+    bot.say(trigger.nick + ', the bot\'s admins are: Reception123, You and 
+else:
     bot.say(trigger.nick + ', the bot\'s admins are: Reception123, SwisterTwister and Zppix.')
 @commands('accesslevel')
 @example('.accesslevel')
@@ -23,11 +23,11 @@ def accesslevel(bot,trigger):
      """
     Finds the accesslevel of the user executing the command
      """
-  if trigger.nick  == 'Reception123' or trigger.nick == 'Reception|away':
-    bot.say('The access level for ' + trigger.nick + ', is Admin.')
-  elif trigger.nick == 'Zppix':
-    bot.say('The access level for ' + trigger.nick + ', is Owner.')
-  elif trigger.nick == 'SwisterTwister' or trigger.nick == 'StormyWaves':
-    bot.say('The access level for ' + trigger.nick + ', is Admin.')
-  else:
-    bot.say('The access level for ' + trigger.nick + ', is User.')
+            if trigger.nick  == 'Reception123' or trigger.nick == 'Reception|away':
+            bot.say('The access level for ' + trigger.nick + ', is Admin.')
+            elif trigger.nick == 'Zppix':
+            bot.say('The access level for ' + trigger.nick + ', is Owner.')
+            elif trigger.nick == 'SwisterTwister' or trigger.nick == 'StormyWaves':
+            bot.say('The access level for ' + trigger.nick + ', is Admin.')
+            else:
+            bot.say('The access level for ' + trigger.nick + ', is User.')
