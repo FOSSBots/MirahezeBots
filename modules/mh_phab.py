91cf3008f1c3f03c72ebf69d766526fb154d6d48
Devin/Zppix <megadev44s.mail@gmail.com>
2017-10-21 13:27:00 -0500

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -3,8 +3,12 @@ import sopel
 import sopel.module
 import requests
 import sopel.tools
-from sopel.module import rule, priority, thread, commands
+from sopel.module import rule, priority, thread, commands, example
 
 @commands('task')
+@example('.task 1')
+'''
+Get a Miraheze phabricator link to a the task number you provide.
+'''
 def phabtask(bot, trigger):
     bot.say('https://phabricator.miraheze.org/T'+ trigger.group(2))
