6d53258fef144a81bde1c45078f664c7382c02a8
Devin/Zppix <megadev44s.mail@gmail.com>
2017-10-21 13:19:26 -0500

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -1,5 +1,10 @@
-from sopel import module
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel
+import sopel.module
+import requests
+import sopel.tools
+from sopel.module import rule, priority, thread, commands
 
-@commands('task', 't')
+@commands('task')
 def phabtask(bot, trigger):
     bot.say('https://www.phabricator.miraheze.org/'+ trigger.group(2))
