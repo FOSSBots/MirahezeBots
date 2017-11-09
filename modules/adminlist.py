0e3b5e1bc9f1dbb11bbb926554eb7c05064c90a4
paladox <paladox@users.noreply.github.com>
2017-11-09 20:49:21 +0000

    Adding paladox as admin

--- modules/adminlist.py
+++ modules/adminlist.py
@@ -8,13 +8,15 @@ from sopel.module import rule, priority, thread, commands
 @commands('botadmins','admins')
 def adminlist(bot, trigger):
   if trigger.nick == 'Reception123':
-    bot.say(trigger.nick + ', the bot\'s admins are: You, SwisterTwister and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, You, SwisterTwister and Zppix.')
   elif trigger.nick == 'Zppix':
-    bot.say(trigger.nick + ', the bot\'s admins are: Reception123, SwisterTwister and you.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister and you.')
   elif trigger.nick == 'SwisterTwister':
-    bot.say(trigger.nick + ', the bot\'s admins are: Reception123, You and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, You and Zppix.')
+  elif trigger.nick == 'paladox':
+    bot.say(trigger.nick + ', the bot\'s admins are: You, Reception123, SwisterTwister and Zppix.')
   else:
-    bot.say(trigger.nick + ', the bot\'s admins are: Reception123, SwisterTwister and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister and Zppix.')
 @commands('accesslevel')
 def accesslevel(bot,trigger):
   if trigger.nick  == 'Reception123':
@@ -22,6 +24,8 @@ def accesslevel(bot,trigger):
   elif trigger.nick == 'Zppix':
       bot.say('The access level for ' + trigger.nick + ', is Owner.')
   elif trigger.nick == 'SwisterTwister':
+        bot.say('The access level for ' + trigger.nick + ', is Admin.')
+  elif trigger.nick == 'paladox':
         bot.say('The access level for ' + trigger.nick + ', is Admin.')
   else:
           bot.say('The access level for ' + trigger.nick + ', is User.')
