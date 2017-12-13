aa9878d8f0a088feec633dfcaafb909266b9aadd
Devin/Zppix <megadev44s.mail@gmail.com>
2017-12-12 18:09:11 -0600

    Update converse.py

--- modules/converse.py
+++ modules/converse.py
@@ -8,7 +8,7 @@ def ping_converse(bot, trigger):
         bot.say("I get it, Reception123, talking to real people is too overrated.")
     elif trigger.nick == 'PuppyKun':
         bot.say("Hit the tab button too soon again, PuppyKun? Tsk tsk tsk.")
-    elif trigger.nick == 'wm-bot' or trigger.nick == 'Source-Zppixbot' or trigger.nick == 'wikibugs' or trigger.nick == 'wikibugs_':
+    elif trigger.nick == 'wm-bot' or trigger.nick == 'wm-bot2' or trigger.nick == 'wm-bot3' or trigger.nick == 'Source-Zppixbot' or trigger.nick == 'wikibugs' or trigger.nick == 'wikibugs_':
         return  
     elif trigger.nick == 'Zppix':
         bot.say("Talking to your own AI bot again, are you Zppix?")
