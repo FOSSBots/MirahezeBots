50430a9b8d0f47f9f17d2c43a25ec5d338ee4d86
Reception123 <utilizator.receptie123@gmail.com>
2017-12-21 17:24:00 +0100

    add version.py (#22)
    * add version.py
    
    Not sure if this is necessary but just in case someone is interested in which version the bot is running us. @Pix1234 if this seems unnecessary feel free to close
    
    * Update version.py
    
    * change version to botversion per request
    
    * Update botversion.py

---
+++ modules/botversion.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel
+import sopel.module
+import requests
+import sopel.tools
+from sopel.module import rule, priority, thread, commands, example
+
+@commands('botversion','bv')
+@example('.botversion')
+ def botversion(bot, trigger):
+   """
+  Lists the current version of the bot
+   """
+   bot.say('The current version of this bot is 2.0 (v2)')
