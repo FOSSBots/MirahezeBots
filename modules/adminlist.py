177ed8007188fe5e6829445b8332a7fd0702319b
Devin/Zppix <megadev44s.mail@gmail.com>
2017-12-22 14:13:01 -0600

    Update adminlist.py

--- modules/adminlist.py
+++ modules/adminlist.py
@@ -8,17 +8,17 @@ from sopel.module import rule, priority, thread, commands
 @commands('botadmins','admins')
 def adminlist(bot, trigger):
   if trigger.nick == 'Reception123':
-    bot.say(trigger.nick + ', the bot\'s admins are: paladox, You, MacFan400, SwisterTwister and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, You, MacFan4000, SwisterTwister and Zppix.')
   elif trigger.nick == 'Zppix':
     bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister and you.')
   elif trigger.nick == 'SwisterTwister':
-    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, MacFan400, You and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, MacFan4000, You and Zppix.')
   elif trigger.nick == 'paladox':
-    bot.say(trigger.nick + ', the bot\'s admins are: You, Reception123, SwisterTwister, MacFan400 and Zppix.')
-  elif trigger.nick == 'MacFan400':
+    bot.say(trigger.nick + ', the bot\'s admins are: You, Reception123, SwisterTwister, MacFan4000 and Zppix.')
+  elif trigger.nick == 'MacFan4000':
     bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister, You, and Zppix.')
   else:
-    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister, MacFan400 and Zppix.')
+    bot.say(trigger.nick + ', the bot\'s admins are: paladox, Reception123, SwisterTwister, MacFan4000 and Zppix.')
 @commands('accesslevel')
 def accesslevel(bot,trigger):
   if trigger.nick  == 'Reception123':
@@ -27,7 +27,7 @@ def accesslevel(bot,trigger):
       bot.say('The access level for ' + trigger.nick + ', is Owner.')
   elif trigger.nick == 'SwisterTwister':
         bot.say('The access level for ' + trigger.nick + ', is Admin.')
-  elif trigger.nick == 'paladox' or trigger.nick == 'MacFan400':
+  elif trigger.nick == 'paladox' or trigger.nick == 'MacFan4000':
         bot.say('The access level for ' + trigger.nick + ', is Admin.')
   else:
           bot.say('The access level for ' + trigger.nick + ', is User.')
