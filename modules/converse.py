3bc694db641fb40e74f6f9665e381f0145584dd9
MacFan4000 <paulfkeffer@gmail.com>
2017-12-23 13:18:37 -0500

    Update converse.py

--- modules/converse.py
+++ modules/converse.py
@@ -11,7 +11,7 @@ def ping_converse(bot, trigger):
 
 @rule('.*request.*wiki.*')
 def ping_miraheze (bot, trigger):
-   if trigger.nick == 'Not-e084':
+   if trigger.nick == 'Not-144f':
     return
    elif trigger.sender == '#miraheze':
     bot.reply("To request a wiki, please see https://meta.miraheze.org/wiki/Special:RequestWiki")
