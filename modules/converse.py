6ea7992e27aba70d70e3a4be427c5c2ba94d3eaa
MacFan4000 <paulfkeffer@gmail.com>
2018-01-04 14:16:49 -0500

    Remove Ping msgs entirely

--- modules/converse.py
+++ modules/converse.py
@@ -3,17 +3,6 @@
 from sopel.module import rule
 
 
-@rule('.*$nickname.*')
-def ping_converse(bot, trigger):
-    """Reply to messages containing this bot name."""
-    if trigger.nick in ['Reception123', 'Reception|away']:
-        bot.say("I get it, Reception123, you want to talk to someone,"
-                "but you shouldn't pick a dumb AI like me, talk to an actual"
-                " person.")
-    else:
-        return
-
-
 @rule('.*request.*wiki.*')
 def reply_to_wiki_request(bot, trigger):
     """Reply to wiki request messages in #miraheze."""
