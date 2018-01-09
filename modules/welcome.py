55d42b3b965e4b2637532a56374f7102f9b7a9c9
Devin/Zppix <megadev44s.mail@gmail.com>
2018-01-09 11:19:53 -0600

    Add #wiki-dev-africa welcome msg

--- modules/welcome.py
+++ modules/welcome.py
@@ -72,6 +72,10 @@ def welcome_user(bot, trigger):
         if trigger.sender == '#miraheze':
             message = ("Hello {}! If you have any questions feel free to ask "
                        "and someone should answer soon.").format(trigger.nick)
+        elif trigger.sender == '#wiki-dev-africa':
+            message = ("Welcome to Africa Wikimedia Developers' channel, {}!"
+                       " If you have any questions feel free to ask"
+                       " Zppix or D3r1ck or anyone who is around!").format(trigger.nick)
         else:
             message = 'Hello {}!'.format(trigger.nick)
 
