9e77f6a7653c8865951a71e3bb7171e81a2fa1e9
MacFan4000 <paulfkeffer@gmail.com>
2018-04-16 17:50:05 -0400

    Spacing

--- modules/welcome.py
+++ modules/welcome.py
@@ -75,7 +75,7 @@ def welcome_user(bot, trigger):
         elif trigger.sender == '#miraheze-cvt':
             message = ("Welcome {}. If you need to report spam or abuse,"
                        "please feel free to notify"
-                       "any of the voiced (+v) users,"
+                       " any of the voiced (+v) users,"
                        "if it contains personal information you can pm them,"
                        "or email us"
                        "at cvt [at] miraheze.org").format(trigger.nick)
