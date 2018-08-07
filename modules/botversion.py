2c99c407aa2d8c0894653df863f2a3e020f69df5
Devin/Zppix <megadev44s.mail@gmail.com>
2018-08-07 10:31:23 -0500

    add link to github source code

--- modules/botversion.py
+++ modules/botversion.py
@@ -15,3 +15,9 @@ from sopel.module import commands, example
 def botversion(bot, trigger):
     """List the current version of the bot."""
     bot.say('The current version of this bot is 3.0 (v3)')
+    
+@commands('source', 'botsource')
+@example('.source')
+def githubsource(bot,trigger):
+    """Give the link to ZppixBot's Github."""
+    bot.reply('My code can be found here: https://github.com/Pix1234/ZppixBot-Source')
