c26dc7d6ca85076f1e6b5efc364dab77292cc8a3
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2019-09-21 16:43:30 -0400

    Add testing module for testing

---
+++ modules/test_module.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""Testing module"""
+
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+import os
+import sopel
+import sopel.module
+import sopel.tools
+
+from sopel.module import commands, require_admin
+
+@commands('testing')
+@require_admin
+def tests(bot, trigger):
+    """Perform tests on the bot. May change or be remove without notice."""
+    if bot.config.ip.GeoIP_db_path:
+        bot.reply(bot.config.ip.GeoIP_db_path)
+        if os.path.isfile(os.join(bot.config.ip.GeoIP_db_path, 'GeoLite2-City.mmdb')):
+            bot.say('And a file exists there too!')
+        else:
+            bot.say('But no file seems to exist!')
+    elif bot.config.core.homedir:
+        bot.reply(bot.config.core.homedir)
+        if os.path.isfile(os.path.join(bot.config.core.homedir, 'GeoLite2-City.mmdb')):
+            bot.say('And a file exists there too!')
+        else:
+            bot.say('But no file seems to exist!')
+    else:
+        bot.reply('/usr/share/GeoIP')
