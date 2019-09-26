5b893f6422a6d57730b4c87b35ef696b4423162a
zppix1 <Megadev44s.mail@gmail.com>
2019-09-26 08:43:13 -0500

    fix travis (again)

--- modules/channelmgnt.py
+++ modules/channelmgnt.py
@@ -15,7 +15,7 @@ import time
 
 from sopel import formatting
 from sopel.module import (
-    commands, example, priority, OP, HALFOP, require_privilege, require_chanmsg
+    commands, example, priority, OP, require_privilege, require_chanmsg
 )
 from sopel.tools import Identifier
 
@@ -28,7 +28,9 @@ def default_mask(trigger):
     arg = formatting.color('{}', formatting.colors.GREEN)
     return '{} {} {} {}'.format(welcome, chan, topic_, arg)
 
+
 chanops = ['Zppix', 'RhinosF1', 'Voidwalker', 'Reception123', 'PuppyKun', 'paladox', 'JohnLewis']
+
 
 @require_chanmsg
 @commands('op')
@@ -39,7 +41,7 @@ def op(bot, trigger):
     """
     if bot.channels[trigger.sender].privileges[bot.nick] < OP and trigger.nick in chanops:
         bot.say('Please wait...')
-        bot.say('op '+ trigger.sender, 'ChanServ')
+        bot.say('op ' + trigger.sender, 'ChanServ')
         time.sleep(1)
     nick = trigger.group(2)
     channel = trigger.sender
@@ -191,7 +193,7 @@ def unban(bot, trigger):
     """
     if bot.channels[trigger.sender].privileges[bot.nick] < OP and trigger.nick in chanops:
         bot.say('Please wait...')
-        bot.say('op '+ trigger.sender, 'ChanServ')
+        bot.say('op ' + trigger.sender, 'ChanServ')
         time.sleep(1)
     text = trigger.group().split()
     argc = len(text)
