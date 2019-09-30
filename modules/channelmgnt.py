fe0c7ae4cae7622b87603d9d18b089a1501ac2f8
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-09-30 22:28:11 +0100

    force string for variables

--- modules/channelmgnt.py
+++ modules/channelmgnt.py
@@ -47,7 +47,7 @@ def op(bot, trigger):
     channel = trigger.sender
     if not nick:
         nick = trigger.nick
-    bot.say('Your nick is: ' + trigger.nick + 'and the chanops are: ' + chanops, trigger.sender)
+    bot.say('Your nick is: ' + str(trigger.nick) + 'and the chanops are: ' + str(chanops), trigger.sender)
     bot.write(['MODE', channel, "+o", nick])
 
 
