ac223cff67555b1d2865e93bdba16adc9c6a079a
zppix1 <Megadev44s.mail@gmail.com>
2019-10-02 16:24:22 -0500

    remove unneeded responses from response.py

--- modules/responses.py
+++ modules/responses.py
@@ -11,64 +11,8 @@ def cancel(bot, trigger):
                "me in their channel").format(trigger.nick))
 
 
-@commands('m')
-@example('.m (nick)')
+@commands('gj', 'gw')
+@example('.gj (nick)')
 def cancel2(bot, trigger):
     """tell the user that they are doing good work."""
     bot.say(("You're doing good work, {}!").format(trigger.group(2)))
-
-
-@rule('update.php')
-def ping_converse(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze':
-        bot.say("Oh no, update.php! If you're not upgrading MediaWiki,"
-                " you should really not be using that!")
-
-
-@rule('my wiki is down')
-def ping_converse2(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze':
-        bot.say("That sounds bad. A sysadmin should be here shortly to"
-                " investigate. If you haven't already, please file a "
-                "Phabricator ticket to facilitate the process!")
-
-
-@rule('Miraheze is down')
-def ping_converse3(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze':
-        bot.say("That sounds bad! A sysadmin should be here shortly to"
-                " investigate. If you haven't already, please file a "
-                "Phabricator ticket to facilitate the process!")
-
-
-@rule('getting 503')
-def ping_converse4(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze' and trigger.nick != 'icinga-miraheze':
-        bot.reply("The servers should return back to normal shortly."
-                  "If that is not the case, please file a task on "
-                  "Phabricator immediately")
-
-
-@rule('The upgrade was successful')
-def ping_converse5(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze':
-        bot.say("Good job %s! Keep on doing what you do.")
-
-
-@rule('Thanks ZppixBot')
-def ping_converse6(bot, trigger):
-    """Reply to message specified in rule."""
-    if trigger.sender == '#miraheze':
-        bot.say('You\'re welcome ' + trigger.nick)
-
-
-@rule('Hi ZppixBot')
-def ping_converse7(bot, trigger):
-    """Reply to message specified in rule."""
-    bot.say('Hi ' + trigger.nick + ', Do you need '
-            'anything from me?')
