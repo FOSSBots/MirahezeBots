d75d3b4912c9bb93585030c29222642bf1f5f70b
zppix1 <Megadev44s.mail@gmail.com>
2019-10-02 16:18:19 -0500

    possible fix 3

--- modules/shortlinks.py
+++ modules/shortlinks.py
@@ -8,4 +8,4 @@ from sopel.module import rule, commands, example
 @commands('github')
 @example('.github repo')
 def ghrepo(bot, trigger):
-    bot.say(("https://github.com/{}").format(trigger.group(2)), trigger.sender())
+    bot.say("https://github.com/" + trigger.group(2))
