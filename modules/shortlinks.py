a6198fb94efcd1ec35be21c3c644e4c69bb24596
zppix1 <Megadev44s.mail@gmail.com>
2019-10-02 16:11:38 -0500

    possible fix 2

--- modules/shortlinks.py
+++ modules/shortlinks.py
@@ -7,5 +7,5 @@ from sopel.module import rule, commands, example
 
 @commands('github')
 @example('.github repo')
-def cancel2(bot, trigger):
+def ghrepo(bot, trigger):
     bot.say(("https://github.com/{}").format(trigger.group(2)))
