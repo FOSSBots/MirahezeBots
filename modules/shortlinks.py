673626c7303a3ed60c86ba69bf2d10cac53fdd0e
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-10-02 22:11:23 +0100

    an idea

--- modules/shortlinks.py
+++ modules/shortlinks.py
@@ -7,5 +7,5 @@ from sopel.module import rule, commands, example
 
 @commands('github')
 @example('.github repo')
-def cancel2(bot, trigger):
+def gh2(bot, trigger):
     bot.say(("https://github.com/{}").format(trigger.group(2)))
