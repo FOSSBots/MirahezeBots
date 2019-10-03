0c2022e348d7eb030253e9f565ee94fd5cefccda
zppix1 <Megadev44s.mail@gmail.com>
2019-10-03 09:07:57 -0500

    fix?

--- modules/responses.py
+++ modules/responses.py
@@ -8,7 +8,7 @@ from sopel.module import rule, commands, example
 def cancel(bot, trigger):
     """Reply to channel request message."""
     bot.reply(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
-               "me in their channel: " + trigger.group(2), '#ZppixBot').format(trigger.nick))
+               "me in their channel: {}", '#ZppixBot').format(trigger.nick, trigger.group(2))
 
 
 @commands('gj', 'gw')
