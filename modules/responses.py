400bb1f693c6ba56df7e6bdfa5eeb2c29b03d26f
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2019-10-03 10:17:21 -0400

    fix

--- modules/responses.py
+++ modules/responses.py
@@ -7,8 +7,8 @@ from sopel.module import rule, commands, example
 @example('.addchannel (insert which)')
 def cancel(bot, trigger):
     """Reply to channel request message."""
-    bot.say("Hey Voidwalker, Reception123 or Zppix, " + trigger.nick " would like to have "
-               "me in their channel: " + trigger.group(2), "#ZppixBot")
+    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
+               "me in their channel: {}").format(trigger.nick, trigger.group(2), '#ZppixBot')
 
 @commands('gj', 'gw')
 @example('.gj (nick)')
