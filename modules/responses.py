33cee415d7de53fe346248f42215799ffeabb9e2
zppix1 <Megadev44s.mail@gmail.com>
2019-10-03 09:04:54 -0500

    make .addchannel always send a message to #ZppixBot and include what channel needs to be added

--- modules/responses.py
+++ modules/responses.py
@@ -7,8 +7,8 @@ from sopel.module import rule, commands, example
 @example('.addchannel (insert which)')
 def cancel(bot, trigger):
     """Reply to channel request message."""
-    bot.reply(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
-               "me in their channel").format(trigger.nick))
+    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
+               "me in their channel: " + trigger.group(2), '#ZppixBot').format(trigger.nick))
 
 
 @commands('gj', 'gw')
