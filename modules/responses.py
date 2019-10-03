dc21b70d88bb1ed7976d544dfc6c6e7bd46ef184
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2019-10-03 10:29:34 -0400

    Fix responses

--- modules/responses.py
+++ modules/responses.py
@@ -1,17 +1,19 @@
 """This module sends responses to frequently posted messages at #miraheze."""
 
-from sopel.module import rule, commands, example
+from sopel.module import commands, example
 
 
 @commands('addchannel')
 @example('.addchannel (insert which)')
 def cancel(bot, trigger):
     """Reply to channel request message."""
-    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
-               "me in their channel: {}").format(trigger.nick, trigger.group(2), '#ZppixBot'))
+    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have " +
+            "me in their channel: {}").format(trigger.nick, trigger.group(2)),
+            '#ZppixBot')
+
 
 @commands('gj', 'gw')
 @example('.gj (nick)')
 def cancel2(bot, trigger):
-    """tell the user that they are doing good work."""
+    """Tell the user that they are doing good work."""
     bot.say(("You're doing good work, {}!").format(trigger.group(2)))
