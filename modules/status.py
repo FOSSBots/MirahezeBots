f9b54bf6bf731a431aa5017e81873e7c7b4f8451
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-11-02 22:55:21 +0000

    reorder code (#95)
    * reorder code
    
    DO NOT MERGE - Will be used to add SUL_GROUP config
    
    * Update status.py
    
    * Update status.py
    
    * Create cloaks.csv
    
    * Update status.py
    
    * Update status.py
    
    * PEP8
    
    using https://codebeautify.org/python-formatter-beautifier
    
    * revert
    
    * fixes
    
    * clean misplaced character
    
    * this better be the last thing

--- modules/status.py
+++ modules/status.py
@@ -12,23 +12,22 @@ from sopel.module import rule, commands, example
 pages = ''
 
 
-def save_wrap(site, requester, status, bot, trigger):
-    pagename = 'User:' + requester + '/Status'
+def save_wrap(site, request, bot, trigger):
+    pagename = 'User:' + request[0] + '/Status'
     bot.say(trigger.nick + " updating " + pagename + "!", trigger.sender)
     page = site.Pages[pagename]
-    content = status
-    save_edit(page, content, bot, trigger)
+    save_edit(page, request[1], bot, trigger)
 
 
-def save_edit(page, content, bot, trigger):
+def save_edit(page, status, bot, trigger):
     time.sleep(5)
-    edit_summary = "BOT: Setting Status to: " + content + " per " + trigger.hostmask
+    edit_summary = "BOT: Setting Status to: " + status + " per " + trigger.hostmask
     times = 0
     while True:
         if times > 1:
             break
         try:
-            page.save(content, summary=edit_summary, bot=True, minor=True)
+            page.save(status, summary=edit_summary, bot=True, minor=True)
             bot.say(trigger.nick + ": Done!", trigger.sender)
         except errors.ProtectedPageError:
             print('Could not edit ' + page + ' due to protection')
@@ -40,25 +39,63 @@ def save_edit(page, content, bot, trigger):
             times += 1
             time.sleep(5)  # sleep for 5 seconds before trying again
             continue
+        except errors.UserBlocked:
+            bot.say(trigger.nick + ": StatusBot is currently unavaiable for that wiki. Our team are working on it!", trigger.sender)
+            bot.say("ERR: The bot is blocked on " + page, '#ZppixBot')
         break
 
 
-def main(wiki, requester, status, bot, trigger):
-    wikiurl = 'example.org'
-    file = open('/data/project/zppixbot/.sopel/modules/config/statuswikis.csv', 'r')
-    for line in file:
-        data = line.split(',')
-        if data[1] == wiki:
-            wikiurl = data[0]
-    site = mwclient.Site(('https', wikiurl), '/w/')
-    config = configparser.RawConfigParser()
-    config.read('/data/project/zppixbot/.sopel/credentials.txt')
-    try:
-        site.login(config.get('zppixbot_status', 'username'), config.get('zppixbot_status', 'password'))
-    except errors.LoginError as e:
-        print(e)
-        raise ValueError("Login failed.")
-    save_wrap(site, requester, status, bot, trigger)
+def main(bot, trigger, options):
+    cont = 0
+    if len(options) == 2:
+        wiki = options[0]
+        status = options[1]
+        host = trigger.host
+        host = host.split('/')
+        cont = 1
+    if len(options) > 2:
+        wiki = options[0]
+        host = trigger.host
+        host = host.split('/')
+        x = 1
+        status = ''
+        while x < len(options):
+            status = status + options[x]
+            x = x + 1
+        cont = 1
+    else:
+        bot.say(trigger.nick + ": Syntax: .mh wikicode status", trigger.sender)
+        cont = 0
+    if cont == 1:
+        cloakfile = open('/data/project/zppixbot/.sopel/modules/config/cloaks.csv', 'r')
+        for line in cloakfile:
+            auth = line.split(',')
+        if host[0] == auth[0]:
+            user = host[1]
+            sulgroup = auth[1]
+            wiki = [wiki, sulgroup]
+            request = [user, status]
+            cont = 1
+        else:
+            bot.say(trigger.nick + ":This service is only avaiable to users with a Miraheze/Wikimedia Cloaks. "
+                    + "See phabricator.wikimedia.org/T234716 for updates.", trigger.sender)
+            cont = 0
+    if cont == 1:
+        wikiurl = 'example.org'
+        file = open('/data/project/zppixbot/.sopel/modules/config/statuswikis.csv', 'r')
+        for line in file:
+            data = line.split(',')
+            if data[1] == wiki[0] and wiki[1] == data[2]:
+                wikiurl = data[0]
+        site = mwclient.Site(('https', wikiurl), '/w/')
+        config = configparser.RawConfigParser()
+        config.read('/data/project/zppixbot/.sopel/credentials.txt')
+        try:
+            site.login(config.get('zppixbot_status', 'username'), config.get('zppixbot_status', 'password'))
+        except errors.LoginError as e:
+            print(e)
+            raise ValueError("Login failed.")
+        save_wrap(site, request, bot, trigger)
 
 
 @commands('status')
@@ -66,16 +103,6 @@ def main(wiki, requester, status, bot, trigger):
 def status(bot, trigger):
     try:
         options = trigger.group(2).split(" ")
-        if len(options) == 2:
-            wiki = options[0]
-            status = options[1]
-            host = trigger.host
-            host = host.split('/')
-            if host[0] == 'miraheze':
-                requester = host[1]
-                main(wiki, requester, status, bot, trigger)
-            else:
-                bot.say(trigger.sender + ": This service is only avaiable to users with a Miraheze/Wikimedia Cloaks. "
-                        + "See phabricator.wikimedia.org/T234716 for updates.")
+        main(bot, trigger, options)
     except AttributeError:
-        bot.say('Syntax: .mh wiki page', trigger.sender)
+        bot.say('Syntax: .mh sitecode status', trigger.sender)
