618fb63bb3d6e7e6db286282cf9fe6a6c0e92ad6
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2019-11-04 13:57:07 -0500

    syntax, spelling, and pep8 fixes

--- modules/status.py
+++ modules/status.py
@@ -1,4 +1,9 @@
-from __future__ import unicode_literals, absolute_import, print_function, division
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division
+)
 import configparser
 import json
 import mwclient
@@ -14,14 +19,16 @@ pages = ''
 
 def save_wrap(site, request, bot, trigger):
     pagename = 'User:' + request[0] + '/Status'
-    bot.say(trigger.nick + ": Updating " + pagename + " to " + request[1] + "!", trigger.sender)
+    bot.say(trigger.nick + ": Updating " + pagename + " to " + request[1]
+            + "!", trigger.sender)
     page = site.Pages[pagename]
     save_edit(page, request[1], bot, trigger)
 
 
 def save_edit(page, status, bot, trigger):
     time.sleep(5)
-    edit_summary = "BOT: Setting Status to:" + status + " per " + trigger.hostmask
+    edit_summary = "BOT: Setting Status to:" + status + " per " \
+                   + trigger.hostmask
     times = 0
     while True:
         if times > 1:
@@ -35,25 +42,31 @@ def save_edit(page, status, bot, trigger):
             times += 1
         except errors.EditError:
             print("Error")
-            bot.say(trigger.nick + ": An Error Occured :(", trigger.sender)
+            bot.say(trigger.nick + ": An Error Occurred :(", trigger.sender)
             times += 1
             time.sleep(5)  # sleep for 5 seconds before trying again
             continue
         except errors.UserBlocked:
-            bot.say(trigger.nick + ": StatusBot is currently unavaiable for that wiki. Our team are working on it!", trigger.sender)
+            bot.say(trigger.nick + ": StatusBot is currently unavailable for "
+                    + "that wiki. Our team are working on it!", trigger.sender)
             bot.say("ERR: The bot is blocked on " + page, '#ZppixBot')
         except requests.exceptions.Timeout:
-            bot.say(trigger.nick + ": We're experinecing delays connecting to that wiki. Try again in a few minutes. If this continues, let us know in #ZppixBot.", trigger.sender)
+            bot.say(trigger.nick + ": We're experiencing delays connecting to "
+                    + "that wiki. Try again in a few minutes. If this "
+                    + "continues, let us know in #ZppixBot.", trigger.sender)
         except requests.exceptions.TooManyRedirects as e:
-            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've alerted a maintainer in #ZppixBot.", trigger.sender)
+            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've "
+                    + "alerted a maintainer in #ZppixBot.", trigger.sender)
             bot.say("Redirect Error: " + e, '#ZppixBot-logs')
-       except requests.exceptions.ConnectionError as e:
-            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've alerted a maintainer in #ZppixBot.", trigger.sender)
-            bot.say("ConnectionError:" + e, '#ZppixBot-logs')
-       except requests.exceptions.RequestException as e:
-            bot.say(trigger.nick + ": A fatal error occured. I've alrted a maintainer in #ZppixBot.", trigger.sender)
-            bot.say("Fatal Error: " + e, '#ZppixBot-logs') 
-       break
+        except requests.exceptions.ConnectionError as e:
+            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've "
+                    + "alerted a maintainer in #ZppixBot.", trigger.sender)
+            bot.say("ConnectionError: " + e, '#ZppixBot-logs')
+        except requests.exceptions.RequestException as e:
+            bot.say(trigger.nick + ": A fatal error occurred. I've alerted a "
+                    + "maintainer in #ZppixBot.", trigger.sender)
+            bot.say("Fatal Error: " + e, '#ZppixBot-logs')
+        break
 
 
 def main(bot, trigger, options):
@@ -75,10 +88,12 @@ def main(bot, trigger, options):
             x = x + 1
         cont = 1
     else:
-        bot.say(trigger.nick + ": Syntax: .mh wikicode status", trigger.sender)
+        bot.say(trigger.nick + ": Syntax: .status wikicode status",
+                trigger.sender)
         cont = 0
     if cont == 1:
-        cloakfile = open('/data/project/zppixbot/.sopel/modules/config/cloaks.csv', 'r')
+        cloakfile = open('/data/project/zppixbot/.sopel/modules/config/'
+                         + 'cloaks.csv', 'r')
         for line in cloakfile:
             auth = line.split(',')
         if host[0] == auth[0]:
@@ -88,16 +103,19 @@ def main(bot, trigger, options):
             request = [user, status]
             cont = 1
         else:
-            bot.say(trigger.nick + ":This service is only avaiable to users with a Miraheze/Wikimedia Cloaks. "
-                    + "See phabricator.wikimedia.org/T234716 for updates.", trigger.sender)
+            bot.say(trigger.nick + ":This service is only available to users "
+                    + "with Miraheze/Wikimedia Cloaks. See"
+                    + "phabricator.wikimedia.org/T234716 for updates.",
+                    trigger.sender)
             cont = 0
     if cont == 1:
         wikiurl = 'example.org'
         wikiexists = 0
-        file = open('/data/project/zppixbot/.sopel/modules/config/statuswikis.csv', 'r')
+        file = open('/data/project/zppixbot/.sopel/modules/config/'
+                    + 'statuswikis.csv', 'r')
         for line in file:
             data = line.split(',')
-            if data[1] == wiki [0]:
+            if data[1] == wiki[0]:
                 wikiexists = 1
             if data[1] == wiki[0] and wiki[1] == data[2]:
                 wikiurl = data[0]
@@ -105,27 +123,39 @@ def main(bot, trigger, options):
                 config = configparser.RawConfigParser()
                 config.read('/data/project/zppixbot/.sopel/credentials.txt')
                 try:
-                    site.login(config.get('zppixbot_status', 'username'), config.get('zppixbot_status', 'password'))
+                    site.login(config.get('zppixbot_status', 'username'),
+                               config.get('zppixbot_status', 'password'))
                 except errors.LoginError as e:
                     print(e)
                     raise ValueError("Login failed.")
                 except requests.exceptions.Timeout:
-                    bot.say(trigger.nick + ": We're experinecing delays connecting to that wiki. Try again in a few minutes. If this continues, let us know in #ZppixBot.", trigger.sender)
+                    bot.say(trigger.nick + ": We're experinecing delays "
+                            + "connecting to that wiki. Try again in a few "
+                            + "minutes. If this continues, let us know in "
+                            + "#ZppixBot.", trigger.sender)
                 except requests.exceptions.TooManyRedirects as e:
-                    bot.say(trigger.nick + ": We couldn't connect to that wiki. I've alerted a maintainer in #ZppixBot.", trigger.sender)
+                    bot.say(trigger.nick + ": We couldn't connect to that "
+                            + "wiki. I've alerted a maintainer in #ZppixBot.",
+                            trigger.sender)
                     bot.say("Redirect Error: " + e, '#ZppixBot-logs')
                 except requests.exceptions.ConnectionError as e:
-                    bot.say(trigger.nick + ": We couldn't connect to that wiki. I've alerted a maintainer in #ZppixBot.", trigger.sender)
-                    bot.say("ConnectionError:" + e, '#ZppixBot-logs')
+                    bot.say(trigger.nick + ": We couldn't connect to that "
+                            + "wiki. I've alerted a maintainer in #ZppixBot.",
+                            trigger.sender)
+                    bot.say("ConnectionError: " + e, '#ZppixBot-logs')
                 except requests.exceptions.RequestException as e:
-                    bot.say(trigger.nick + ": A fatal error occured. I've alrted a maintainer in #ZppixBot.", trigger.sender)
-                    bot.say("Fatal Error: " + e, '#ZppixBot-logs') 
+                    bot.say(trigger.nick + ": A fatal error occured. I've "
+                            + "alerted a maintainer in #ZppixBot.",
+                            trigger.sender)
+                    bot.say("Fatal Error: " + e, '#ZppixBot-logs')
                 save_wrap(site, request, bot, trigger)
                 cont = 0
-        if cont = 1 and wikiexists = 1:
-            bot.say(trigger.nick + ": I couldn't authentice you for that wiki.", trigger.sender)
-        elif wikiexists = 0:
-            bot.say(trigger.nick ": I don't recongise that wiki.", trigger.sender)
+        if cont == 1 and wikiexists == 1:
+            bot.say(trigger.nick + ": I couldn't authentice you for that "
+                    + "wiki.", trigger.sender)
+        elif wikiexists == 0:
+            bot.say(trigger.nick + ": I don't recongise that wiki.",
+                    trigger.sender)
 
 
 @commands('status')
