ac414143d08b814d3fde4c3a07a4b56ba0a0f7ca
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-01-16 22:52:23 +0000

    hmm

--- modules/welcome.py
+++ modules/welcome.py
@@ -67,7 +67,7 @@ def welcome_user(bot, trigger):
 
     if trigger.sender not in bot.known_users_list:
         bot.known_users_list[trigger.sender] = []
-    bot.say(trigger.account + trigger.nick, '#ZppixBot-logs')
+    bot.say(str(trigger.account) + str(trigger.nick), '#ZppixBot-logs')
     if str(trigger.account) not in bot.known_users_list[trigger.sender] or str(trigger.nick) not in bot.known_users_list[trigger.sender]:
         if trigger.sender == '#miraheze':
             message = ("Hello {}! If you have any questions, feel free to ask "
