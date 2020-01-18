25732ba8128ce157acf77c0379bce71703b6c9ac
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-01-18 22:30:51 +0000

    Update welcome.py

--- modules/welcome.py
+++ modules/welcome.py
@@ -103,7 +103,7 @@ def welcome_user(bot, trigger):
 
     if trigger.sender not in bot.known_users_list:
         bot.known_users_list[trigger.sender] = []
-    if trigger.acount is not none:
+    if trigger.account is not none:
         if trigger.account not in bot.known_users_list[trigger.sender] and trigger.nick not in bot.known_users_list[trigger.sender]:
             send_welcome(bot,trigger)
     else:
