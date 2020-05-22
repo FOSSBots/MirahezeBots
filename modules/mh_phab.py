b129fad24243561aa6be9e43011ed96b223de40e
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-05-22 14:18:10 +0100

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -105,7 +105,7 @@ def phabtask(bot, trigger):
 @rule('T[1-9][0-9]*')
 def phabtask2(bot, trigger):
     """Get a Miraheze phabricator link to a the task number you provide."""
-    searchphab(bot, trigger)
+    bot.say("If you're expecting info on phab task to show up, nag RhinosF1 to fix this and use .task", trigger.sender)
 
 
 @interval(HIGHPRIO_TASKS_NOTIFICATION_INTERVAL)
