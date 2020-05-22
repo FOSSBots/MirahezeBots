5d673427b05f357d51da28e6ae01080cd343cee9
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-05-22 10:45:06 +0100

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -10,13 +10,14 @@ MESSAGES_INTERVAL = 2  # seconds (to avoid excess flood)
 startup_tasks_notifications = False
 priotasks_notify = []
 
+
 def searchphab(bot, trigger):
     data = {
         'api.token': config.phabricator.api_token,
         'constraints[ids][0]': trigger.group(2)
     }
     response = requests.post(
-        url='https://'+config.phabricator.host+'/api/maniphest.search',
+        url='https://' + config.phabricator.host + '/api/maniphest.search',
         data=data)
     response = response.json()
     result = response["result"]["data"][0]
@@ -25,7 +26,7 @@ def searchphab(bot, trigger):
         'constraints[phids][0]': result["fields"]["ownerPHID"]
     }
     response2 = requests.post(
-        url='https://'+config.phabricator.host+'/user.search',
+        url='https://' + config.phabricator.host + '/user.search',
         data=params)
     response2 = response2.json()
     params2 = {
@@ -33,7 +34,7 @@ def searchphab(bot, trigger):
         'constraints[phids][0]': result["fields"]["authorPHID"]
     }
     response3 = requests.post(
-        url='https://'+config.phabricator.host+'/api/user.search',
+        url='https://' + config.phabricator.host + '/api/user.search',
         data=params2)
     response3 = response3.json()
     owner = response2["result"]["data"][0]["fields"]["username"]
@@ -41,29 +42,30 @@ def searchphab(bot, trigger):
     output = "https://phabricator.miraheze.org/T" + str(result["id"]) + " - " + str(
         result["fields"]["name"] + ", authored by " + author + ", assigned to " + str(owner))
     bot.say(output, trigger.sender)
-    
+
+
 def gethighpri(limit=True, channel='#miraheze', bot=None):
     data = {
         'api.token': config.phabricator.api_token,
         'queryKey': config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
     }
     response = requests.post(
-        url='https://'+config.phabricator.host+'/api/mainphest.search',
+        url='https://' + config.phabricator.host + '/api/mainphest.search',
         data=data)
     response = response.json()
-        result = response["result"]
-        data = result["data"]
-        x = 0
-        while x < len(data):
-          currdata = data[x]
-          if x > 5 and limit == True:
-              bot.say("They are more than 5 tasks. Please see " + config.phabricator.host + " for the rest or use .highpri", channel)
-              break
-          else:
-              params = {
+    result = response["result"]
+    data = result["data"]
+    x = 0
+    while x < len(data):
+        currdata = data[x]
+        if x > 5 and limit is True:
+            bot.say("They are more than 5 tasks. Please see " + config.phabricator.host + " for the rest or use .highpri", channel)
+            break
+        else:
+            params = {
                   'api.token': config.phabricator.api_token,
                   'constraints[phids][0]': currdata["fields"]["ownerPHID"],
-              }
+            }
               response2 = requests.post(
                   url='https://' + config.phabricator.host + '/api/user.search', 
                   data=params)
@@ -81,12 +83,13 @@ def gethighpri(limit=True, channel='#miraheze', bot=None):
               output = "https://phabricator.miraheze.org/T" + str(currdata["id"]) + " - " + str(currdata["fields"]["name"] + ", authored by " + author + ", assigned to " + str(owner))
               bot.say(output, channel)
               x = x + 1
-    
-    
+ 
+
 @commands('task')
 @example('.task 1')
 def phabtask(bot, trigger):
     searchphab(bot, trigger)
+
 
 @rule('T[1-9][0-9]*')	
 def phabtask2(bot, trigger):	
@@ -98,7 +101,8 @@ def phabtask2(bot, trigger):
 def high_priority_tasks_notification(bot):
     """Send high priority tasks notifications."""
     gethighpri(bot=bot)
-    
+
+
 @commands('highpri')
 @example('.highpri')
 def forcehighpri(bot, trigger):
