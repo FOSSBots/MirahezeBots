63ee8499fdf71c9d3e68ef042e7bcb2e6981e6c7
examknow <ek311@outlook.com>
2020-05-27 17:14:37 -0500

    add first regex rule

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -7,7 +7,7 @@ from __future__ import (
     division
 )
 
-from sopel.module import commands, example
+from sopel.module import commands, example, rule
 
 MIRAHEZE_ABOUT_MIRAHEZE_CHANNEL = (
     'Miraheze is a non-profit wikifarm running MediaWiki. If you would like '
@@ -40,13 +40,24 @@ def miraheze(bot, trigger):
 def miraheze_gethelp(bot, trigger):
     """Reply to help requests."""
     if trigger.sender == '#miraheze':
-        bot.reply(trigger.nick + ', needs help. Pinging Reception123, Zppix, '
+        bot.say(trigger.nick + ', needs help. Pinging Reception123, Zppix, '
                 'PuppyKun, Voidwalker, RhinosF1.')
-    else:
-        bot.reply('If you need Miraheze releated help, please join #miraheze')
 
 
 @commands('discord')
 def miraheze_discord(bot, trigger):
     """Displays discord information for Miraheze."""
     bot.reply('You can join discord by going to, https://discord.is/miraheze!')
+    
+@rule('.*[w-wW-W]hat (even is [m-mM-M]iraheze|is [m-mM-M]iraheze|does [m-mM-M]iraheze do).*')
+def miraheze(bot, trigger):
+    """
+    Miraheze about command.
+
+    This command will tell you about Miraheze and where to learn more.
+    """
+    if trigger.sender == '#miraheze':
+        bot.reply(MIRAHEZE_ABOUT_MIRAHEZE_CHANNEL)
+    else:
+        bot.reply(MIRAHEZE_ABOUT_OTHER_CHANNELS)
+
