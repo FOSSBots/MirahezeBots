4fb66b345eff9cca6460bbc02ab84ea41e5a940d
examknow <ek311@outlook.com>
2020-05-28 11:47:16 -0500

    shut up travis

--- modules/miraheze.py
+++ modules/miraheze.py
@@ -1,5 +1,4 @@
 """This module contains #miraheze-specific commands."""
-
 from __future__ import (
     unicode_literals,
     absolute_import,
@@ -42,7 +41,7 @@ def miraheze(bot, trigger):
 def miraheze_gethelp(bot, trigger):
     """Reply to help requests."""
     if trigger.sender == '#miraheze':
-        bot.reply('Pinging Reception123, Zppix, PuppyKun, Voidwalker, RhinosF1, and Examknow who might be able to help you.')                                                    
+        bot.reply('Pinging Reception123, Zppix, PuppyKun, Voidwalker, RhinosF1, and Examknow who might be able to help you.')
     else:
          bot.reply('If you need Miraheze releated help, please join #miraheze')
 
