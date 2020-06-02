a562b4a72cf15bc46806005d9c26e002eb3359ab
RhinosF1 <rhinosf1@gmail.com>
2020-06-02 10:38:07 +0100

    Update dbclean.py

--- dbclean.py
+++ dbclean.py
@@ -4,9 +4,9 @@ with open(file, 'r') as f: # ensure the file is open and closed properly
     users = f.readlines()
 database = input("Full path to database: ")
 with sqlite3.connect(database) as conn:
-    with conn.cursor() as curs:
-        for user in users:
-            curs.execute('DELETE FROM nick_values WHERE nick_id = ?', user)
-            curs.execute('DELETE FROM nicknames WHERE nick_id = ?', user)
-            curs.execute('DELETE FROM nick_ids WHERE nick_id = ?', user)
-        conn.commit()
+    curs = conn.cursor()
+    for user in users:
+        curs.execute('DELETE FROM nick_values WHERE nick_id = ?', user)
+        curs.execute('DELETE FROM nicknames WHERE nick_id = ?', user)
+        curs.execute('DELETE FROM nick_ids WHERE nick_id = ?', user)
+    conn.commit()
