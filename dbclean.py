9110c6507108a672250d99f21edc3e8f8b4c2ebd
RhinosF1 <rhinosf1@gmail.com>
2020-06-02 10:17:41 +0100

    Create Database CleanUp Script (#172)

---
+++ dbclean.py
@@ -0,0 +1,11 @@
+file = input("Full path to the deletion list: ")
+with open(file, 'r') as f: # ensure the file is open and closed properly
+    users = f.readlines()
+database = input("Full path to database: ")
+with sqlite3.connect(database) as conn:
+    with conn.cursor() as curs:
+        for user in users:
+            curs.execute('DELETE FROM nick_values WHERE nick_id = ?', user)
+            curs.execute('DELETE FROM nicknames WHERE nick_id = ?', user)
+            curs.execute('DELETE FROM nick_ids WHERE nick_id = ?', user)
+        conn.commit()
