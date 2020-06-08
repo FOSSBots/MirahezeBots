e1ebe72a46043e39b2a5a82b040175aa841e74e4
MacFan4000 <paulfkeffer@gmail.com>
2020-06-08 13:04:45 -0400

    add rate limits (#191)
    * add rate limits
    
    * fixes
    
    * Update responses.py

--- modules/responses.py
+++ modules/responses.py
@@ -1,13 +1,15 @@
 """This module sends responses to frequently posted messages at #miraheze."""
 
-from sopel.module import commands, example
+from sopel.module import commands, example, rate, require_account
 
 
 @commands('addchannel')
 @example('.addchannel (insert which)')
+@rate(user=120, channel=240, server=60)
+@require_account()
 def addchan(bot, trigger):
     """Reply to channel request message."""
-    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
+    bot.say(("Hey MacFan4000, RhinosF1, Texas, Voidwalker, Reception123 or Zppix, {} would like to have "
             + "me in their channel: {}").format(trigger.nick, trigger.group(2)),
             '#ZppixBot')
     if trigger.sender != '#ZppixBot':
@@ -16,6 +18,7 @@ def addchan(bot, trigger):
 
 @commands('gj', 'gw')
 @example('.gj (nick)')
+@rate(user=2, channel=1, server=0)
 def gj(bot, trigger):
     """Tell the user that they are doing good work."""
     bot.say(("You're doing good work, {}!").format(trigger.group(2)))
@@ -23,6 +26,7 @@ def gj(bot, trigger):
 
 @commands('cancelreminder')
 @example('.cancelreminder (insert reminder message here)')
+@rate(user=2, channel=1, server=0)
 def cancel(bot, trigger):
     """Cancel reminder."""
     bot.reply(('Pinging RhinosF1, Reception123, Voidwalker or Zppix to cancel '
@@ -31,6 +35,8 @@ def cancel(bot, trigger):
 
 @commands('botversion', 'bv')
 @example('.botversion')
+@require_account()
+@rate(user=2, channel=1, server=0)
 def botversion(bot, trigger):
     """List the current version of the bot."""
     bot.say('The current version of this bot is 6.0 (v6)')
@@ -38,6 +44,7 @@ def botversion(bot, trigger):
 
 @commands('source', 'botsource')
 @example('.source')
+@rate(user=2, channel=1, server=0)
 def githubsource(bot, trigger):
     """Give the link to ZppixBot's Github."""
     bot.reply('My code can be found here: https://github.com/Pix1234/ZppixBot-Source')
