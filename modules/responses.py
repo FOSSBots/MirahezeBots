87c59d938fa11f9fa8442c4e3e8aa02093ef9d80
MacFan4000 <paulfkeffer@gmail.com>
2020-06-08 12:52:23 -0400

    add rate limits

--- modules/responses.py
+++ modules/responses.py
@@ -5,9 +5,10 @@ from sopel.module import commands, example
 
 @commands('addchannel')
 @example('.addchannel (insert which)')
+@rate(user=120,channel=240,server=60)
 def addchan(bot, trigger):
     """Reply to channel request message."""
-    bot.say(("Hey Voidwalker, Reception123 or Zppix, {} would like to have "
+    bot.say(("Hey MacFan4000, RhinosF!, Texas, Voidwalker, Reception123 or Zppix, {} would like to have "
             + "me in their channel: {}").format(trigger.nick, trigger.group(2)),
             '#ZppixBot')
     if trigger.sender != '#ZppixBot':
@@ -16,6 +17,7 @@ def addchan(bot, trigger):
 
 @commands('gj', 'gw')
 @example('.gj (nick)')
+@rate(user=2,channel=1,server=0)
 def gj(bot, trigger):
     """Tell the user that they are doing good work."""
     bot.say(("You're doing good work, {}!").format(trigger.group(2)))
@@ -23,6 +25,7 @@ def gj(bot, trigger):
 
 @commands('cancelreminder')
 @example('.cancelreminder (insert reminder message here)')
+@rate(user=2,channel=1,server=0)
 def cancel(bot, trigger):
     """Cancel reminder."""
     bot.reply(('Pinging RhinosF1, Reception123, Voidwalker or Zppix to cancel '
@@ -31,6 +34,7 @@ def cancel(bot, trigger):
 
 @commands('botversion', 'bv')
 @example('.botversion')
+@rate(user=2,channel=1,server=0)
 def botversion(bot, trigger):
     """List the current version of the bot."""
     bot.say('The current version of this bot is 6.0 (v6)')
@@ -38,6 +42,7 @@ def botversion(bot, trigger):
 
 @commands('source', 'botsource')
 @example('.source')
+@rate(user=2,channel=1,server=0)
 def githubsource(bot, trigger):
     """Give the link to ZppixBot's Github."""
     bot.reply('My code can be found here: https://github.com/Pix1234/ZppixBot-Source')
