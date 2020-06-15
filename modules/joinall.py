b4c8fdf576f22460919945248ded0c856eb5dc4c
MacFan4000 <paulfkeffer@gmail.com>
2020-06-15 11:52:17 -0400

    Update and rename join.py to joinall.py

--- modules/joinall.py
+++ modules/joinall.py
@@ -1,4 +1,4 @@
-"""This module implements channel joining on startup to fix a bug."""
+"""This module implements .joinall."""
 
 from __future__ import (
     unicode_literals,
@@ -19,19 +19,6 @@ def handle_joins(bot, trigger):
     """Join some channels."""
     channels = bot.config.core.channels
     if trigger.sender == '#ZppixBot':
-        for channel in channels:
-            bot.join(channel)
-            time.sleep(1)
-
-
-@module.rule('.*')
-@module.event('JOIN')
-@module.thread(True)
-@module.unblockable
-def handle_joins_auto(bot, trigger):
-    """Join some channels automatically."""
-    channels = bot.config.core.channels
-    if trigger.nick == bot.nick and trigger.sender == '#ZppixBot':
         for channel in channels:
             bot.join(channel)
             time.sleep(1)
