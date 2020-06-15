e671e21b7839cb31345a973e9b56696222fb5857
MacFan4000 <paulfkeffer@gmail.com>
2020-06-15 11:20:14 -0400

    fixes

--- modules/join.py
+++ modules/join.py
@@ -11,14 +11,13 @@ from sopel import module
 
 import time
 
-channels = bot.config.core.channels
-
 
 @module.require_admin
 @module.commands('joinall')
 @module.thread(True)
 def handle_joins(bot, trigger):
     """Join some channels."""
+    channels = bot.config.core.channels
     if trigger.sender == '#ZppixBot':
         for channel in channels:
             bot.join(channel)
@@ -31,7 +30,8 @@ def handle_joins(bot, trigger):
 @module.unblockable
 def handle_joins_auto(bot, trigger):
     """Join some channels automatically."""
+    channels = bot.config.core.channels
     if trigger.nick == bot.nick and trigger.sender == '#ZppixBot':
-        for chan in chans:
-            bot.join(chan)
+        for channel in channels:
+            bot.join(channel)
             time.sleep(1)
