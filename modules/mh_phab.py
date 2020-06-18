10c2c86b81b55afaadeb056afed02009e6e8b534
RhinosF1 <rhinosf1@gmail.com>
2020-06-18 10:52:59 +0100

    Revert "Merge pull request #199 from Pix1234/version-7"
    This reverts commit 127f6627f7eef75764adfec018ec3dca2892903f, reversing
    changes made to ed503109004039d5b462fa8b009656ca5e77a6a9.
    
    Rollback Version 7 Upgrade

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -1,130 +1,226 @@
 """This module contains commands related to Miraheze Phabricator."""
 
-import json  # FIX THIS
-import requests  # FIX THIS
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division
+)
 
-from sopel import config
-from sopel.module import commands, example, interval, rule
 import sys
+import os
+import re
 
-BOLD = '\x02'
+from time import time, sleep
+
+from sopel.module import commands, example, interval, rule
+
+# sopel modules import problem workaround
+sys.path.insert(0, os.path.abspath(__file__ + "/.."))
+from utils.phabricator import PhabricatorClient
+
 HIGHPRIO_NOTIF_TASKS_PER_PAGE = 5
 HIGHPRIO_TASKS_NOTIFICATION_INTERVAL = 7 * 24 * 60 * 60  # every week
 MESSAGES_INTERVAL = 2  # seconds (to avoid excess flood)
 startup_tasks_notifications = False
 priotasks_notify = []
-config = config.Config('/data/project/zppixbot/.sopel/default.cfg')
 
 
-def searchphab(bot, channel, task=1):
-    data = {
-        'api.token': config.phabricator.api_token,
-        'constraints[ids][0]': task
-    }
-    response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(config.phabricator.host),
-        data=data)
-    response = response.json()
-    go = 0
-    try:
-        result = response.get("result").get("data")[0]
-        go = 1
-    except AttributeError:
-        bot.say("An error occurred while parsing the result.", channel)
-    except IndexError:
-        bot.say("Sorry, but I couldn't find information for the task you searched.", channel)
-    except:
-        bot.say("An unknown error occured.", channel)
-    if go == 1:
-        params = {
-            'api.token': config.phabricator.api_token,
-            'constraints[phids][0]': result.get("fields").get("ownerPHID")
-        }
-        response2 = requests.post(
-            url='https://{0}/api/user.search'.format(config.phabricator.host),
-            data=params)
-        try:
-            response2 = response2.json()
-        except json.decoder.JSONDecodeError as e:
-            bot.say(response2.text, '#ZppixBot-Logs')
-            bot.say(str(e), '#ZppixBot-Logs')
-        params2 = {
-            'api.token': config.phabricator.api_token,
-            'constraints[phids][0]': result.get("fields").get("authorPHID")
-        }
-        response3 = requests.post(
-            url='https://{0}/api/user.search'.format(config.phabricator.host),
-            data=params2)
-        response3 = response3.json()
-        if result.get("fields").get("ownerPHID") is None:
-            owner = None
-        else:
-            owner = response2.get("result").get("data")[0].get("fields").get("username")
-        author = response3.get("result").get("data")[0].get("fields").get("username")
-        priority = result.get("fields").get("priority").get("name")
-        status = result.get("fields").get("status").get("name")
-        output = 'https://phabricator.miraheze.org/T{0} - '.format(str(result["id"]))
-        output = '{0}{2}{1}{2}, '.format(output, str(result.get('fields').get('name')), BOLD)
-        output = output + 'authored by {1}{0}{1}, '.format(author, BOLD)
-        output = output + 'assigned to {1}{0}{1}, '.format(owner, BOLD)
-        output = output + 'Priority: {1}{0}{1}, '.format(priority, BOLD)
-        output = output + 'Status: {1}{0}{1}'.format(status, BOLD)
-        bot.say(output, channel)
+def setup(bot):
+    """Setup phabricator client."""
+    global priotasks_notify
+    if hasattr(bot.config, 'phabricator'):
+        bot.phabricator = PhabricatorClient(
+            bot.config.phabricator.host,
+            bot.config.phabricator.api_token
+        )
+
+        if (hasattr(bot.config.phabricator, 'priotasks_notify') and bot.config.phabricator.priotasks_notify is not None):
+            priotasks_notify = list(map(
+                lambda f: f.strip(),
+                bot.config.phabricator.priotasks_notify.split(',')
+            ))
 
 
-def gethighpri(limit=True, channel='#miraheze', bot=None):
-    data = {
-        'api.token': config.phabricator.api_token,
-        'queryKey': config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
-    }
-    response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(config.phabricator.host),
-        data=data)
-    response = response.json()
-    result = response.get("result")
-    try:
-        data = result.get("data")
-        go = 1
-    except:
-        bot.say("They are no high priority tasks that I can process, good job!", channel)
-        go = 0
-    if go == 1:
-        x = 0
-        while x < len(data):
-            currdata = data[x]
-            if x > 5 and limit:
-                bot.say("They are more than 5 tasks. Please see {0} for the rest or use .highpri".format(
-                    config.phabricator.host), channel)
-                break
-            else:
-                searchphab(bot=bot, channel=channel, task=currdata.get("id"))
-                x = x + 1
+def mass_message(bot, targets, message):
+    """Send the same message to multiple targets."""
+    for target in targets:
+        bot.say(message, target)
+        sleep(MESSAGES_INTERVAL)
 
 
 @commands('task')
 @example('.task 1')
 def phabtask(bot, trigger):
-    if trigger.group(2).startswith('T'):
-        task_id = trigger.group(2).split('T')[1]
-    else:
-        task_id = trigger.group(2)
-    searchphab(bot=bot, channel=trigger.sender, task=task_id)
-
-
-@rule('T[1-9][0-9]*')
-def phabtask2(bot, trigger):
     """Get a Miraheze phabricator link to a the task number you provide."""
-    task_id = trigger.split('T')[1]
-    searchphab(bot=bot, channel=trigger.sender, task=task_id)
+    task_id = int(re.sub("[^0-9]", "", trigger.group(2)))
+
+    if not hasattr(bot, 'phabricator'):
+        # Fallback for case if phabricator is not set up
+        bot.say('https://phabricator.miraheze.org/T{}'.format(task_id))
+        return
+
+    task = bot.phabricator.get_task(task_id)
+    if task is None:
+        bot.reply('I can\'t find task with id {}'.format(task_id))
+        return
+
+    message = '{} - {} [{}] authored by {}'.format(
+        task.link,
+        task.title,
+        task.status,
+        task.author.username
+    )
+    if task.owner is not None:
+        message += ', assigned to {}'.format(task.owner.username)
+    else:
+        message += ', assigned to None'
+    bot.say(message)
+
+
+@commands('priotasks')
+@example('.priotasks 2')
+def high_priority_tasks_no_updates(bot, trigger):
+    """Command to find high priority tasks whithout updates for a while."""
+    if trigger.account not in bot.config.core.admin_accounts:
+        bot.reply('Only bot admins can search Phabricator tasks.')
+        return
+
+    tasks = bot.phabricator.find_tasks(
+        priorities=[PhabricatorClient.PRIORITY_HIGH],
+        statuses=[PhabricatorClient.STATUS_OPEN]
+    )
+
+    if len(tasks) == 0:
+        bot.reply('There are no high priority open tasks. Nice job!')
+        return
+
+    page_number = 1
+    if trigger.group(3):
+        page_number = int(re.sub('[^0-9]', '', trigger.group(3)))
+    if page_number <= 0:
+        bot.reply('Invalid page number')
+        return
+
+    page_offset = (page_number - 1) * HIGHPRIO_NOTIF_TASKS_PER_PAGE
+    tasks_waiting = 0
+    page_overflow_tasks = 0
+
+    for task in tasks:
+        time_diff = int(round((time() - task.dateModified) / (24 * 60 * 60)))
+        if time_diff < 3:
+            continue
+        if page_offset > 0:
+            page_offset -= 1
+            continue
+        if tasks_waiting >= HIGHPRIO_NOTIF_TASKS_PER_PAGE:
+            page_overflow_tasks += 1
+            continue
+
+        message = 'No updates for {} - {} - {} - authored by {}'.format(
+            str(time_diff) + ' days',
+            task.link,
+            task.title,
+            task.author.username
+        )
+        if task.owner is not None:
+            message += ', assigned to {}'.format(task.owner.username)
+        else:
+            message += ', assigned to None'
+        bot.say(message)
+        sleep(MESSAGES_INTERVAL)
+
+        tasks_waiting += 1
+
+    if tasks_waiting == 0:
+        if page_number == 0:
+            bot.reply('No tasks with last update time > 3 days. Good job!')
+        else:
+            bot.reply('No tasks on this page')
+    elif page_overflow_tasks != 0:
+        bot.say('and {} more (see page {}...)'.format(
+            page_overflow_tasks,
+            page_number + 1
+        ))
 
 
 @interval(HIGHPRIO_TASKS_NOTIFICATION_INTERVAL)
 def high_priority_tasks_notification(bot):
     """Send high priority tasks notifications."""
-    gethighpri(bot=bot)
+    tasks = bot.phabricator.find_tasks(
+        priorities=[PhabricatorClient.PRIORITY_HIGH],
+        statuses=[PhabricatorClient.STATUS_OPEN]
+    )
+
+    if len(tasks) == 0:
+        mass_message(bot, priotasks_notify, 'Hi! Just contacting you to tell '
+                     'that here are no high priority open tasks. Nice job!')
+        return
+
+    tasks_waiting = 0
+    page_overflow_tasks = 0
+
+    for task in tasks:
+        time_diff = int(round((time() - task.dateModified) / (24 * 60 * 60)))
+        if time_diff < 3:
+            continue
+        if tasks_waiting >= HIGHPRIO_NOTIF_TASKS_PER_PAGE:
+            page_overflow_tasks += 1
+            continue
+
+        if tasks_waiting == 0:
+            mass_message(bot, priotasks_notify, 'Hi! Here is the list of '
+                         'currently open high priority tasks on Phabricator')
+
+        message = 'No updates for {} - {} - {} - authored by {}'.format(
+            str(time_diff) + ' days',
+            task.link,
+            task.title,
+            task.author.username
+        )
+        if task.owner is not None:
+            message += ', assigned to {}'.format(task.owner.username)
+        else:
+            message += ', assigned to None'
+
+        mass_message(bot, priotasks_notify, message)
+
+        tasks_waiting += 1
+
+    if tasks_waiting == 0:
+        mass_message(bot, priotasks_notify, 'Hi! Just contacting you to tell '
+                     'that here are no high priority open tasks with last '
+                     'update time > 3 days. Nice job!')
+    elif page_overflow_tasks != 0:
+        mass_message(bot, priotasks_notify,
+                     'and {} more (see next pages...)'.format(
+                         page_overflow_tasks
+                     ))
 
 
-@commands('highpri')
-@example('.highpri')
-def forcehighpri(bot, trigger):
-    gethighpri(limit=False, channel=trigger.sender, bot=bot)
+@rule('T[1-9][0-9]*')
+def phabtask2(bot, trigger):
+    """Get a Miraheze phabricator link to a the task number you provide."""
+    task_id = int(re.sub("[^0-9]", "", trigger))
+
+    if not hasattr(bot, 'phabricator'):
+        # Fallback for case if phabricator is not set up
+        bot.say('https://phabricator.miraheze.org/T{}'.format(task_id))
+        return
+
+    task = bot.phabricator.get_task(task_id)
+    if task is None:
+        bot.reply('I can\'t find task with id {}'.format(task_id))
+        return
+
+    message = '{} - {} [{}] authored by {}'.format(
+        task.link,
+        task.title,
+        task.status,
+        task.author.username
+    )
+    if task.owner is not None:
+        message += ', assigned to {}'.format(task.owner.username)
+    else:
+        message += ', assigned to None'
+    bot.say(message)
