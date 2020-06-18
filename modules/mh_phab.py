8cbd698cb743def74a5b688e53d350dad7a8bd4f
RhinosF1 <rhinosf1@gmail.com>
2020-06-18 10:43:34 +0100

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -15,11 +15,11 @@ priotasks_notify = []
 
 def searchphab(bot, channel, task=1):
     data = {
-        'api.token': config.phabricator.api_token,
+        'api.token': bot.config.phabricator.api_token,
         'constraints[ids][0]': task
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(config.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
         data=data)
     response = response.json()
     go = 0
@@ -34,11 +34,11 @@ def searchphab(bot, channel, task=1):
         bot.say("An unknown error occured.", channel)
     if go == 1:
         params = {
-            'api.token': config.phabricator.api_token,
+            'api.token': bot.config.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("ownerPHID")
         }
         response2 = requests.post(
-            url='https://{0}/api/user.search'.format(config.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
             data=params)
         try:
             response2 = response2.json()
@@ -46,11 +46,11 @@ def searchphab(bot, channel, task=1):
             bot.say(response2.text, '#ZppixBot-Logs')
             bot.say(str(e), '#ZppixBot-Logs')
         params2 = {
-            'api.token': config.phabricator.api_token,
+            'api.token': bot.config.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("authorPHID")
         }
         response3 = requests.post(
-            url='https://{0}/api/user.search'.format(config.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
             data=params2)
         response3 = response3.json()
         if result.get("fields").get("ownerPHID") is None:
@@ -71,11 +71,11 @@ def searchphab(bot, channel, task=1):
 
 def gethighpri(limit=True, channel='#miraheze', bot=None):
     data = {
-        'api.token': config.phabricator.api_token,
-        'queryKey': config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
+        'api.token': bot.config.phabricator.api_token,
+        'queryKey': bot.config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(config.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
         data=data)
     response = response.json()
     result = response.get("result")
@@ -91,7 +91,7 @@ def gethighpri(limit=True, channel='#miraheze', bot=None):
             currdata = data[x]
             if x > 5 and limit:
                 bot.say("They are more than 5 tasks. Please see {0} for the rest or use .highpri".format(
-                    config.phabricator.host), channel)
+                    bot.config.phabricator.host), channel)
                 break
             else:
                 searchphab(bot=bot, channel=channel, task=currdata.get("id"))
