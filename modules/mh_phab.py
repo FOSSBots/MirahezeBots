db548d1211fc9698ee2183bc59805ddea08f8158
RhinosF1 <rhinosf1@gmail.com>
2020-06-21 23:02:07 +0100

    Update mh_phab.py

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -31,7 +31,6 @@ def searchphab(bot, channel, task=1):
         go = 1
     except AttributeError:
         bot.say("An error occurred while parsing the result.", channel)
-        bot.say(response.text, "#ZppixBot-logs")
     except IndexError:
         bot.say("Sorry, but I couldn't find information for the task you searched.", channel)
     except:
