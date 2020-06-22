9ad971fb34674b7ffd846c33203c213ff25c39c7
MacFan4000 <paulfkeffer@gmail.com>
2020-06-22 19:07:43 -0400

    make less ZppixBot specific and general cleanup (#206)
    * make less ZppixBot specific and general cleanup
    
    also fixed a deprecation warning
    
    * fixes for travis
    
    * more fixes
    
    * more fixes
    
    * more fixes
    
    * fix
    
    * urgh
    
    * fix?
    
    * fix?
    
    * why
    
    * Update status.py
    
    * try to fix
    
    * fixes
    
    * fixes
    
    * Fix
    
    * Update status.py
    
    * Oh
    
    * fix syntax errors
    
    * Update status.py
    
    * Update status.py
    
    Co-authored-by: The-Voidwalker <voidwalker.wikipedia@gmail.com>

--- modules/status.py
+++ modules/status.py
@@ -1,26 +1,43 @@
-from __future__ import (
-    unicode_literals,
-    absolute_import,
-    print_function,
-    division
-)
+from __future__ import unicode_literals, absolute_import, print_function, division
+
 import configparser
 import json
 import mwclient
-from mwclient import errors
 import requests
 import re
 import time
+
+from mwclient import errors
+
 from sopel.module import rule, commands, example
+from sopel.config.types import StaticSection, ValidatedAttribute
 
 
 pages = ''
 
 
+class StatusSection(StaticSection):
+    data_path = ValidatedAttribute('data_path', str)
+    wiki_username = ValidatedAttribute('wiki_username', str)
+    wiki_password = ValidatedAttribute('wiki_username', str)
+    support_channel = ValidatedAttribute('support_channel', str)
+
+
+def setup(bot):
+    bot.config.define_section('status', StatusSection)
+
+
+def configure(config):
+    config.define_section('status', StatusSection, validate=False)
+    config.status.configure_setting('data_path', 'What is the path to the statusbot data files?')
+    config.status.configure_setting('wiki_username', 'What is the statusbot wiki username? (from Special:BotPasswords)')
+    config.status.configure_setting('wiki_password', 'What is the statusbot wiki password? (from Special:BotPasswords)')
+    config.status.configure_setting('data_path', 'Specify a support IRC channel (leave blank for none).')
+
+
 def save_wrap(site, request, bot, trigger):
     pagename = 'User:' + request[0] + '/Status'
-    bot.say(trigger.nick + ": Updating " + pagename + " to " + request[1]
-            + "!", trigger.sender)
+    bot.reply("Updating " + pagename + " to " + request[1] + "!")
     page = site.Pages[pagename]
     save_edit(page, request[1], bot, trigger)
 
@@ -35,37 +52,43 @@ def save_edit(page, status, bot, trigger):
             break
         try:
             page.save(status, summary=edit_summary, bot=True, minor=True)
-            bot.say(trigger.nick + ": Updated!", trigger.sender)
+            bot.reply("Updated!")
         except errors.ProtectedPageError:
             print('Could not edit ' + page + ' due to protection')
-            bot.say(trigger.nick + ": Error: Page Protected", trigger.sender)
+            bot.reply("Error: Page Protected")
             times += 1
         except errors.EditError:
             print("Error")
-            bot.say(trigger.nick + ": An Error Occurred :(", trigger.sender)
+            bot.reply("An Error Occurred :(")
             times += 1
             time.sleep(5)  # sleep for 5 seconds before trying again
             continue
         except errors.UserBlocked:
-            bot.say(trigger.nick + ": StatusBot is currently unavailable for "
-                    + "that wiki. Our team are working on it!", trigger.sender)
-            bot.say("ERR: The bot is blocked on " + str(page), '#ZppixBot-logs')
+            bot.reply("StatusBot is currently unavailable for that wiki. Our team are working on it!")
+            bot.say("ERR: The bot is blocked on " + str(page), 'bot.config.core.logging_channel')
         except requests.exceptions.Timeout:
-            bot.say(trigger.nick + ": We're experiencing delays connecting to "
-                    + "that wiki. Try again in a few minutes. If this "
-                    + "continues, let us know in #ZppixBot.", trigger.sender)
+            bot.reply("We're experinecing delays "
+                      + "connecting to that wiki. Try again in a few minutes.")
+            if bot.config.status.support_channel is not None:
+                bot.say("If this continues, let us know in {}".format(bot.config.status.support_channel))
         except requests.exceptions.TooManyRedirects as e:
-            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've "
-                    + "alerted a maintainer in #ZppixBot.", trigger.sender)
-            bot.say("Redirect Error: " + e, '#ZppixBot-logs')
+            bot.reply("We couldn't connect to that wiki.")
+            if bot.config.status.support_channel is not None:
+                bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+            print(e)
+            raise ValueError("Redirect error")
         except requests.exceptions.ConnectionError as e:
-            bot.say(trigger.nick + ": We couldn't connect to that wiki. I've "
-                    + "alerted a maintainer in #ZppixBot.", trigger.sender)
-            bot.say("ConnectionError: " + e, '#ZppixBot-logs')
+            bot.reply("We couldn't connect to that wiki.")
+            if bot.config.status.support_channel is not None:
+                bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+            print(e)
+            raise ValueError("Connection error")
         except requests.exceptions.RequestException as e:
-            bot.say(trigger.nick + ": A fatal error occurred. I've alerted a "
-                    + "maintainer in #ZppixBot.", trigger.sender)
-            bot.say("Fatal Error: " + e, '#ZppixBot-logs')
+            bot.reply("A fatal error occured.")
+            if bot.config.status.support_channel is not None:
+                bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+            print(e)
+            raise ValueError("Fatal error")
         break
 
 
@@ -88,12 +111,11 @@ def main(bot, trigger, options):
             x = x + 1
         cont = 1
     else:
-        bot.say(trigger.nick + ": Syntax: .status wikicode status",
-                trigger.sender)
+        bot.reply("Syntax: .status wikicode status")
         cont = 0
     if cont == 1:
         cont = 0
-        cloakfile = open('/data/project/zppixbot-test/.sopel/modules/config/'
+        cloakfile = open(bot.config.status.data_path
                          + 'cloaks.csv', 'r')
         for line in cloakfile:
             auth = line.split(',')
@@ -105,7 +127,7 @@ def main(bot, trigger, options):
                 cont = 1
                 break
         if cont == 0:
-            usersfile = open('/data/project/zppixbot-test/.sopel/modules/config/'
+            usersfile = open(bot.config.status.data_path
                              + 'users.csv', 'r')
             for line in usersfile:
                 auth = line.split(',')
@@ -117,14 +139,15 @@ def main(bot, trigger, options):
                     cont = 1
                     break
         if cont == 0:
-            bot.say(trigger.nick + ": You don't seem to be authorised to use this module."
-                    + "Please check you are signed into NickServ and try again. If this"
-                    + "persists, ask for help in #ZppixBot", trigger.sender)
+            bot.reply("You don't seem to be authorised to use this module."
+                      + "Please check you are signed into NickServ and try again.", trigger.sender)
+            if bot.config.status.support_channel is not None:
+                bot.say("If this persists, ask for help in {}".format(bot.config.status.support_channel))
             cont = 0
     if cont == 1:
         wikiurl = 'example.org'
         wikiexists = 0
-        file = open('/data/project/zppixbot-test/.sopel/modules/config/'
+        file = open(bot.config.status.data_path
                     + 'statuswikis.csv', 'r')
         for line in file:
             data = line.split(',')
@@ -132,43 +155,42 @@ def main(bot, trigger, options):
                 wikiexists = 1
             if data[1] == wiki[0] and wiki[1] == data[2]:
                 wikiurl = data[0]
-                site = mwclient.Site(('https', wikiurl), '/w/')
-                config = configparser.RawConfigParser()
-                config.read('/data/project/zppixbot-test/.sopel/credentials.txt')
+                site = mwclient.Site((wikiurl), '/w/')
                 try:
-                    site.login(config.get('zppixbot_status', 'username'),
-                               config.get('zppixbot_status', 'password'))
+                    site.login(bot.config.status.wiki_username,
+                               bot.config.status.wiki_password)
                 except errors.LoginError as e:
                     print(e)
                     raise ValueError("Login failed.")
                 except requests.exceptions.Timeout:
-                    bot.say(trigger.nick + ": We're experinecing delays "
-                            + "connecting to that wiki. Try again in a few "
-                            + "minutes. If this continues, let us know in "
-                            + "#ZppixBot.", trigger.sender)
+                    bot.reply("We're experinecing delays "
+                              + "connecting to that wiki. Try again in a few minutes.")
+                    if bot.config.status.support_channel is not None:
+                        bot.say("If this continues, let us know in {}".format(bot.config.status.support_channel))
                 except requests.exceptions.TooManyRedirects as e:
-                    bot.say(trigger.nick + ": We couldn't connect to that "
-                            + "wiki. I've alerted a maintainer in #ZppixBot.",
-                            trigger.sender)
-                    bot.say("Redirect Error: " + e, '#ZppixBot-logs')
+                    bot.reply("We couldn't connect to that wiki.")
+                    if bot.config.status.support_channel is not None:
+                        bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+                    print(e)
+                    raise ValueError("Redirect error")
                 except requests.exceptions.ConnectionError as e:
-                    bot.say(trigger.nick + ": We couldn't connect to that "
-                            + "wiki. I've alerted a maintainer in #ZppixBot.",
-                            trigger.sender)
-                    bot.say("ConnectionError: " + e, '#ZppixBot-logs')
+                    bot.reply("We couldn't connect to that wiki.")
+                    if bot.config.status.support_channel is not None:
+                        bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+                    print(e)
+                    raise ValueError("Connection error")
                 except requests.exceptions.RequestException as e:
-                    bot.say(trigger.nick + ": A fatal error occured. I've "
-                            + "alerted a maintainer in #ZppixBot.",
-                            trigger.sender)
-                    bot.say("Fatal Error: " + e, '#ZppixBot-logs')
+                    bot.reply("A fatal error occured.")
+                    if bot.config.status.support_channel is not None:
+                        bot.say("I've alerted a maintainer in {}".format(bot.config.status.support_channel))
+                    print(e)
+                    raise ValueError("Fatal error")
                 save_wrap(site, request, bot, trigger)
                 cont = 0
         if cont == 1 and wikiexists == 1:
-            bot.say(trigger.nick + ": I couldn't authentice you for that "
-                    + "wiki.", trigger.sender)
+            bot.reply("I couldn't authentice you for that wiki.")
         elif wikiexists == 0:
-            bot.say(trigger.nick + ": I don't recongise that wiki.",
-                    trigger.sender)
+            bot.reply("I don't recongise that wiki.")
 
 
 @commands('status')
