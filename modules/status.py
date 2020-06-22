d1ac780b252b0a5e707b4b8bf982c3d2fc2cf8ce
MacFan4000 <paulfkeffer@gmail.com>
2020-06-22 15:54:39 -0400

    fix?

--- modules/status.py
+++ modules/status.py
@@ -1,9 +1,4 @@
-from __future__ import (
-    unicode_literals,
-    absolute_import,
-    print_function,
-    division
-)
+from __future__ import unicode_literals, absolute_import, print_function, division
 import configparser
 import json
 import mwclient
