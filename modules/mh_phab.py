5cd5bbad3fc5673b1bb74c3867ace1100db6b52e
MacFan4000 <paulfkeffer@gmail.com>
2020-06-24 13:10:44 -0400

    fixs

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -33,7 +33,6 @@ def configure(config):
 
 BOLD = '\x02'
 HIGHPRIO_NOTIF_TASKS_PER_PAGE = 5
-HIGHPRIO_TASKS_NOTIFICATION_INTERVAL = bot.config.phabricator.highpri_notify_interval
 MESSAGES_INTERVAL = 2  # seconds (to avoid excess flood)
 startup_tasks_notifications = False
 priotasks_notify = []
@@ -41,11 +40,11 @@ priotasks_notify = []
 
 def searchphab(bot, channel, task=1):
     data = {
-        'api.token': bot.config.phabricator.api_token,
+        'api.token': bot.settings.phabricator.api_token,
         'constraints[ids][0]': task
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.settings.phabricator.host),
         data=data)
     response = response.json()
     go = 0
@@ -60,11 +59,11 @@ def searchphab(bot, channel, task=1):
         bot.say("An unknown error occured.", channel)
     if go == 1:
         params = {
-            'api.token': bot.config.phabricator.api_token,
+            'api.token': bot.settings.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("ownerPHID")
         }
         response2 = requests.post(
-            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.settings.phabricator.host),
             data=params)
         try:
             response2 = response2.json()
@@ -72,11 +71,11 @@ def searchphab(bot, channel, task=1):
             bot.say(response2.text, '#ZppixBot-Logs')
             bot.say(str(e), '#ZppixBot-Logs')
         params2 = {
-            'api.token': bot.config.phabricator.api_token,
+            'api.token': bot.settings.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("authorPHID")
         }
         response3 = requests.post(
-            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.settings.phabricator.host),
             data=params2)
         response3 = response3.json()
         if result.get("fields").get("ownerPHID") is None:
@@ -95,13 +94,13 @@ def searchphab(bot, channel, task=1):
         bot.say(output, channel)
 
 
-def gethighpri(limit=True, channel=bot.config.highpri_channel, bot=None):
+def gethighpri(limit=True, channel, bot=None):
     data = {
-        'api.token': bot.config.phabricator.api_token,
-        'queryKey': bot.config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
+        'api.token': bot.settings.phabricator.api_token,
+        'queryKey': bot.settings.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.settings.phabricator.host),
         data=data)
     response = response.json()
     result = response.get("result")
@@ -117,7 +116,7 @@ def gethighpri(limit=True, channel=bot.config.highpri_channel, bot=None):
             currdata = data[x]
             if x > 5 and limit:
                 bot.say("They are more than 5 tasks. Please see {0} for the rest or use .highpri".format(
-                    bot.config.phabricator.host), channel)
+                    bot.settings.phabricator.host), )
                 break
             else:
                 searchphab(bot=bot, channel=channel, task=currdata.get("id"))
@@ -141,11 +140,11 @@ def phabtask2(bot, trigger):
     searchphab(bot=bot, channel=trigger.sender, task=task_id)
 
 
-@interval(HIGHPRIO_TASKS_NOTIFICATION_INTERVAL)
+@interval(bot.settings.phabricator.highpri_notify_interval)
 def high_priority_tasks_notification(bot):
-    if bot.config.phabricator.highpri_notify == true:
+    if bot.settings.phabricator.highpri_notify is True:
         """Send high priority tasks notifications."""
-        gethighpri(bot=bot)
+        gethighpri(channel=bot.settings.phabricator.highpri_channel, bot=bot)
 
 
 @commands('highpri')
