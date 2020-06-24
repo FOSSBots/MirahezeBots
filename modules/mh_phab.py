768fb1a32faa403beac6c88698f733c2d960d96e
MacFan4000 <paulfkeffer@gmail.com>
2020-06-24 12:40:50 -0400

    make less ZppixBot specific, overhaul configurability

--- modules/mh_phab.py
+++ modules/mh_phab.py
@@ -1,13 +1,39 @@
-"""This module contains commands related to Miraheze Phabricator."""
+"""This module contains commands related to Phabricator."""
 
 import json  # FIX THIS
 import requests  # FIX THIS
 from sopel.module import commands, example, interval, rule
+from sopel.config.types import StaticSection, ValidatedAttribute
 import sys
+
+
+class PhabricatorSection(StaticSection):
+    host = ValidatedAttribute('host', str)
+    api_token = ValidatedAttribute('api_token', str)
+    querykey = ValidatedAttribute('querykey', str)
+    highpri_notify = ValidatedAttribute('highpri_notify', bool)
+    highpri_channel = ValidatedAttribute('highpri_channel', str)
+    highpri_notify_interval = ValidatedAttribute('highpri_notify_interval', int)
+
+
+def setup(bot):
+    bot.config.define_section('phabricator', PhabricatorSection)
+
+
+def configure(config):
+    config.define_section('phabricator', PhabricatorSection, validate=False)
+    config.phabricator.configure_setting('host', 'What is the URL of your Phabricator installation?')
+    config.phabricator.configure_setting('api_token', 'Please enter a Phabricator API token.')
+    config.phabricator.configure_setting('querykey', 'Please enter a Phabricator query key.')
+    config.phabricator.configure_setting('highpri_notify', 'Would you like to enable automatic notification of high priority tasks? (true/false)')
+    config.phabricator.configure_setting('highpri_channel', 'If you enabled high priority notifications, what channel would you like them sent to?')
+    config.phabricator.configure_setting('highpri_notify_interval',
+                                         'If you enabled high priority notifications, how often (in secconds) would you like notifications to be sent?')
+
 
 BOLD = '\x02'
 HIGHPRIO_NOTIF_TASKS_PER_PAGE = 5
-HIGHPRIO_TASKS_NOTIFICATION_INTERVAL = 7 * 24 * 60 * 60  # every week
+HIGHPRIO_TASKS_NOTIFICATION_INTERVAL = bot.config.phabricator.highpri_notify_interval
 MESSAGES_INTERVAL = 2  # seconds (to avoid excess flood)
 startup_tasks_notifications = False
 priotasks_notify = []
@@ -15,11 +41,11 @@ priotasks_notify = []
 
 def searchphab(bot, channel, task=1):
     data = {
-        'api.token': bot.settings.phabricator.api_token,
+        'api.token': bot.config.phabricator.api_token,
         'constraints[ids][0]': task
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(bot.settings.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
         data=data)
     response = response.json()
     go = 0
@@ -34,11 +60,11 @@ def searchphab(bot, channel, task=1):
         bot.say("An unknown error occured.", channel)
     if go == 1:
         params = {
-            'api.token': bot.settings.phabricator.api_token,
+            'api.token': bot.config.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("ownerPHID")
         }
         response2 = requests.post(
-            url='https://{0}/api/user.search'.format(bot.settings.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
             data=params)
         try:
             response2 = response2.json()
@@ -46,11 +72,11 @@ def searchphab(bot, channel, task=1):
             bot.say(response2.text, '#ZppixBot-Logs')
             bot.say(str(e), '#ZppixBot-Logs')
         params2 = {
-            'api.token': bot.settings.phabricator.api_token,
+            'api.token': bot.config.phabricator.api_token,
             'constraints[phids][0]': result.get("fields").get("authorPHID")
         }
         response3 = requests.post(
-            url='https://{0}/api/user.search'.format(bot.settings.phabricator.host),
+            url='https://{0}/api/user.search'.format(bot.config.phabricator.host),
             data=params2)
         response3 = response3.json()
         if result.get("fields").get("ownerPHID") is None:
@@ -69,13 +95,13 @@ def searchphab(bot, channel, task=1):
         bot.say(output, channel)
 
 
-def gethighpri(limit=True, channel='#miraheze', bot=None):
+def gethighpri(limit=True, channel=bot.config.highpri_channel, bot=None):
     data = {
-        'api.token': bot.settings.phabricator.api_token,
-        'queryKey': bot.settings.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
+        'api.token': bot.config.phabricator.api_token,
+        'queryKey': bot.config.phabricator.querykey,  # mFzMevK.KRMZ for mhphab
     }
     response = requests.post(
-        url='https://{0}/api/maniphest.search'.format(bot.settings.phabricator.host),
+        url='https://{0}/api/maniphest.search'.format(bot.config.phabricator.host),
         data=data)
     response = response.json()
     result = response.get("result")
@@ -91,7 +117,7 @@ def gethighpri(limit=True, channel='#miraheze', bot=None):
             currdata = data[x]
             if x > 5 and limit:
                 bot.say("They are more than 5 tasks. Please see {0} for the rest or use .highpri".format(
-                    bot.settings.phabricator.host), channel)
+                    bot.config.phabricator.host), channel)
                 break
             else:
                 searchphab(bot=bot, channel=channel, task=currdata.get("id"))
@@ -117,8 +143,9 @@ def phabtask2(bot, trigger):
 
 @interval(HIGHPRIO_TASKS_NOTIFICATION_INTERVAL)
 def high_priority_tasks_notification(bot):
-    """Send high priority tasks notifications."""
-    gethighpri(bot=bot)
+    if bot.config.phabricator.highpri_notify == true:
+        """Send high priority tasks notifications."""
+        gethighpri(bot=bot)
 
 
 @commands('highpri')
