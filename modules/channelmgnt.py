89ca504593fa5a4ca151ab6511be616762e77b75
MacFan4000 <paulfkeffer@gmail.com>
2020-06-30 11:46:31 -0400

    + #miraheze-botlogs

--- modules/channelmgnt.py
+++ modules/channelmgnt.py
@@ -55,6 +55,8 @@ def get_chanops(bot, trigger):
         chanops = ['Zppix', 'tom29739', 'SwisterTwister', 'Snowycats', 'DatGuy', 'mattwj2002']
     elif str(trigger.sender) == '#ays':
         chanops = ['Mbrt', 'Arifys', 'Murbaut']
+    elif str(trigger.sender) == '#miraheze-botlogs':
+        chanops = ['MacFan4000', 'Zppix', 'RhinosF1', 'Reception123', 'Voidwalker']
     else:
         bot.say('Please ask in #ZppixBot for assistance configuring the channel management module.', trigger.sender)
     return chanops
