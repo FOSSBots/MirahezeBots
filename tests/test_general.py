eeafbfb9e2d426aecf22e8d7ff75f66891927c28
RhinosF1 <rhinosf1@gmail.com>
2020-07-06 10:35:34 +0100

    Update test_general.py

--- tests/test_general.py
+++ tests/test_general.py
@@ -2,7 +2,8 @@ import os
 import re
 import sqlite3
 import sys
-PATH = '../MirahezeBots/modules'
+PATH = '../MirahezeBots'
+MODULEPATH = '../MirahezeBots/modules'
 sys.path.append(PATH)
 from sqlalchemy import create_engine
 import models
@@ -24,22 +25,22 @@ def test_db_schema_is_same():
     assert original == set(engine.table_names()) 
 
 def test_no_f_strings():
-    for top, dirs, files in os.walk(PATH):
+    for top, dirs, files in os.walk(MODULEPATH):
         for filen in files:
             if not filen.endswith('.py'):
                 continue
-            with open(os.path.join(PATH, filen)) as python_source:
+            with open(os.path.join(MODULEPATH, filen)) as python_source:
                 src = python_source.read()
                 assert "f'" not in src
                 assert 'f"' not in src
 
 def test_line_length():
     MAX_LENGTH = 220+1
-    for top, dirs, files in os.walk(PATH):
+    for top, dirs, files in os.walk(MODULEPATH):
         for filen in files:
             if not filen.endswith('.py'):
                 continue
-            with open(os.path.join(PATH, filen)) as python_source:
+            with open(os.path.join(MODULEPATH, filen)) as python_source:
                 src = python_source.readlines()
                 for line_number, line in enumerate(src):
                     assert len(line.strip()) < MAX_LENGTH, 'length of line #{0} exceeds limit'.format(line_number)
@@ -50,10 +51,10 @@ def future_test_db_cleanup():
 
 def test_no_get_on_lists():
     reg = r'get\([0-9]'
-    for top, dirs, files in os.walk(PATH):
+    for top, dirs, files in os.walk(MODULEPATH):
         for filen in files:
             if not filen.endswith('.py'):
                 continue
-            with open(os.path.join(PATH, filen)) as python_source:
+            with open(os.path.join(MODULEPATH, filen)) as python_source:
                 src = python_source.read()
                 assert not re.search(reg, src) 
