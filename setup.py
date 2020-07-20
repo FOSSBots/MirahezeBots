80a793ba475e2603e3d4a6c13cf5dcb8b0e0be1a
RhinosF1 <rhinosf1@gmail.com>
2020-07-20 13:41:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    print('Sopel does not correctly load modules installed with setup.py '
+          'directly. Please use "pip install .", or add {}/sopel_modules to '
+          'core.extra in your config.'.format(
+              os.path.dirname(os.path.abspath(__file__))),
+          file=sys.stderr)
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('NEWS') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = [req for req in requirements_file.readlines()]
+
+with open('tests/requirements.txt') as dev_requirements_file:
+    dev_requirements = [req for req in dev_requirements_file.readlines()]
+
+
+setup(
+    name='MirahezeBot_Plugins',
+    version='8.0.0',
+    description='Sopel Plugins for Miraheze Bots',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',  # This is important!
+    author='MirahezeBot Contributors',
+    author_email='bots@miraheze.org',
+    url='https://github.com/MirahezeBots/MirahezeBots',
+    packages=find_packages('.'),
+    namespace_packages=['MirahezeBots'],
+    include_package_data=True,
+    install_requires=requirements,
+    tests_require=dev_requirements,
+    test_suite='tests',
+    license='Eiffel Forum License, version 2',
+)
