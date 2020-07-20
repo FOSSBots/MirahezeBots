dd9685dda405777a699e23e2f046c7183a57c256
RhinosF1 <rhinosf1@gmail.com>
2020-07-20 11:35:55 +0100

    Fix format

--- MirahezeBots/__init__.py
+++ MirahezeBots/__init__.py
@@ -1,1 +1,16 @@
-__import__('pkg_resources').declare_namespace(__name__)
+# coding=utf8
+"""MirahezeBot Plugins
+Custom Sopel Plugins For Miraheze Bots
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+try:
+    from .youtube import *
+except ImportError:
+    # probably being imported by setup.py to get metadata before installation
+    # no cause for alarm
+    pass
+
+__author__ = 'MirahezeBot Contributors'
+__email__ = 'bots@miraheze.org'
+__version__ = '8'
