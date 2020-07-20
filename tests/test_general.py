6179c7786f22cdc82a4a892efffc70624ae8a93f
RhinosF1 <rhinosf1@gmail.com>
2020-07-20 10:52:19 +0100

    Update test_general.py

--- tests/test_general.py
+++ tests/test_general.py
@@ -3,7 +3,7 @@ import re
 import sqlite3
 import sys
 PATH = '../MirahezeBots'
-MODULEPATH = '../MirahezeBots/modules'
+PLUGINPATH = '../MirahezeBots/plugins'
 sys.path.append(PATH)
 from sqlalchemy import create_engine
 import models
@@ -26,11 +26,11 @@ def test_db_schema_is_same():
 
 def test_line_length():
     MAX_LENGTH = 220+1
-    for top, dirs, files in os.walk(MODULEPATH):
+    for top, dirs, files in os.walk(PLUGINPATH):
         for filen in files:
             if not filen.endswith('.py'):
                 continue
-            with open(os.path.join(MODULEPATH, filen)) as python_source:
+            with open(os.path.join(PLUGINPATH, filen)) as python_source:
                 src = python_source.readlines()
                 for line_number, line in enumerate(src):
                     assert len(line.strip()) < MAX_LENGTH, 'length of line #{0} exceeds limit'.format(line_number)
@@ -41,10 +41,10 @@ def future_test_db_cleanup():
 
 def test_no_get_on_lists():
     reg = r'get\([0-9]'
-    for top, dirs, files in os.walk(MODULEPATH):
+    for top, dirs, files in os.walk(PLUGINPATH):
         for filen in files:
             if not filen.endswith('.py'):
                 continue
-            with open(os.path.join(MODULEPATH, filen)) as python_source:
+            with open(os.path.join(PLUGINPATH, filen)) as python_source:
                 src = python_source.read()
                 assert not re.search(reg, src) 
