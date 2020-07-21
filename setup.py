7e51a67abb9903eec52cf7c19b6e71b1d6f6b846
RhinosF1 <rhinosf1@gmail.com>
2020-07-21 11:02:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,6 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-
-if __name__ == '__main__':
-    print('Sopel does not correctly load modules installed with setup.py '
-          'directly. Please use "pip install .", or add {}/sopel_modules to '
-          'core.extra in your config.'.format(
-              os.path.dirname(os.path.abspath(__file__))),
-          file=sys.stderr)
-
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
