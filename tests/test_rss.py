be86d5a49a8a01fe1f06dd1d1c6994327b008708
MacFan4000 <paulfkeffer@gmail.com>
2020-07-24 10:55:34 -0400

    try somthing

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from sopel.db import SopelDB
-from MirahezeBot-Plugins import sopel.plugins-rss
+import MirahezeBot-Plugins
 from sopel.test_tools import MockSopel, MockConfig
 import hashlib
 import os
