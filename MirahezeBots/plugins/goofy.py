427a728f899269ae98690a9d188a1f6e2b6e1cd9
JohnBishop95 <68743815+JohnBishop95@users.noreply.github.com>
2020-07-24 11:13:38 -0500

    fixing pep8 issues

--- MirahezeBots/plugins/goofy.py
+++ MirahezeBots/plugins/goofy.py
@@ -3,6 +3,7 @@
 
 from sopel import module
 
+
 @module.example('.coffee JohnBishop')
 @module.commands('coffee')
 def coffee(bot, trigger):
@@ -10,8 +11,9 @@ def coffee(bot, trigger):
     Makes me give the specified nick a coffee.
     """
     if trigger.group(2) == '':
-      bot.reply("To whom should I give this cup of coffee?")
+        bot.reply("To whom should I give this cup of coffee?")
     bot.action("gives %s a nice warm cup of coffee" % (trigger.group(2)), trigger.sender)
+
 
 @module.example('.hug JohnBishop')
 @module.commands('hug')
@@ -20,9 +22,10 @@ def coffee(bot, trigger):
     Makes me give the specified nick a hug.
     """
     if trigger.group(2) == '':
-      bot.reply("To whom should I give this hug?")
+        bot.reply("To whom should I give this hug?")
     bot.action("gives %s a great big bear hug" % (trigger.group(2)), trigger.sender)
-    
+
+
 @module.example('.burger JohnBishop')
 @module.commands('burger')
 def coffee(bot, trigger):
@@ -30,8 +33,9 @@ def coffee(bot, trigger):
     Makes me give the specified nick a burger.
     """
     if trigger.group(2) == '':
-      bot.reply("To whom should I give this cheeseburger?")
+        bot.reply("To whom should I give this cheeseburger?")
     bot.action("gives %s a freshly cooked cheeseburger" % (trigger.group(2)), trigger.sender)
+
 
 @module.example('.present JohnBishop')
 @module.commands('present')
@@ -40,5 +44,5 @@ def coffee(bot, trigger):
     Makes me give the specified nick a present.
     """
     if trigger.group(2) == '':
-      bot.reply("To whom should I give this present?")
+        bot.reply("To whom should I give this present?")
     bot.action("gives %s a present" % (trigger.group(2)), trigger.sender)
