62f5dce643bda4639956c255e7ef1b2a98c8dac8
JohnBishop95 <68743815+JohnBishop95@users.noreply.github.com>
2020-07-24 10:52:05 -0500

    add examples

--- MirahezeBots/plugins/goofy.py
+++ MirahezeBots/plugins/goofy.py
@@ -3,24 +3,28 @@
 
 from sopel import module
 
+@module.example('.coffee JohnBishop')
 @module.commands('coffee')
 def coffee(bot, trigger):
     if trigger.group(2) == '':
       bot.reply("To whom should I give this cup of coffee?")
     bot.action("gives %s a nice warm cup of coffee" % (trigger.group(2)), trigger.sender)
 
+@module.example('.hug JohnBishop')
 @module.commands('hug')
 def coffee(bot, trigger):
     if trigger.group(2) == '':
       bot.reply("To whom should I give this hug?")
     bot.action("gives %s a great big bear hug" % (trigger.group(2)), trigger.sender)
     
+@module.example('.burger JohnBishop')
 @module.commands('burger')
 def coffee(bot, trigger):
     if trigger.group(2) == '':
       bot.reply("To whom should I give this cheeseburger?")
     bot.action("gives %s a freshly cooked cheeseburger" % (trigger.group(2)), trigger.sender)
-    
+
+@module.example('.present JohnBishop')
 @module.commands('present')
 def coffee(bot, trigger):
     if trigger.group(2) == '':
