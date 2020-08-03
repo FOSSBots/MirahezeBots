6760a7c3401d7d701e6c4ced5215c603e92da672
RhinosF1 <rhinosf1@gmail.com>
2020-08-03 10:44:25 +0100

    Update phab.py

--- MirahezeBots/plugins/phab.py
+++ MirahezeBots/plugins/phab.py
@@ -138,7 +138,7 @@ def phabtask(bot, trigger):
 @rule('T[1-9][0-9]*')
 def phabtask2(bot, trigger):
     """Get a Miraheze phabricator link to a the task number you provide."""
-    task_id = trigger.match.group(1)
+    task_id = trigger.match.group(0)
     bot.reply(task_id)
     task_id = trigger.match.split('T')[1]
     searchphab(bot=bot, channel=trigger.sender, task=task_id)
