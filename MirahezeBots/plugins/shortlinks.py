d63e148fa9cae14d4e486a4867b7287e9a787fbb
RhinosF1 <rhinosf1@gmail.com>
2020-08-07 09:48:10 +0100

    shortlinks: drop unused imports

--- MirahezeBots/plugins/shortlinks.py
+++ MirahezeBots/plugins/shortlinks.py
@@ -1,8 +1,7 @@
 """This module expands links to various websites"""
 from __future__ import unicode_literals, absolute_import, print_function, division
 
-import re
-from sopel.module import rule, commands, example
+from sopel.module import commands, example
 
 
 @commands('github', 'gh')
