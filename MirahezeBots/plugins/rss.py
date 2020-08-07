cb105b5db6800286deb20840d626eb92cde00152
RhinosF1 <rhinosf1@gmail.com>
2020-08-07 09:58:42 +0100

    rss: remove future

--- MirahezeBots/plugins/rss.py
+++ MirahezeBots/plugins/rss.py
@@ -6,7 +6,6 @@ Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3
 
 This module posts rss feed items to irc channels
 """
-from __future__ import unicode_literals
 from sopel.config.types import StaticSection, ListAttribute, ValidatedAttribute
 from sopel.logger import get_logger
 from sopel.module import commands, interval, require_admin, example
