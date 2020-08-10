66d6eb03b4b177597d11a9f176fd29f771b930b0
RhinosF1 <rhinosf1@gmail.com>
2020-08-10 22:20:18 +0100

    jsonparser: add cache validation

--- MirahezeBots/utils/jsonparser.py
+++ MirahezeBots/utils/jsonparser.py
@@ -5,3 +5,11 @@ def createdict(filename):
     with open(filename) as jsonfile:
         data = jsonfile.read()
     return json.loads(data)
+
+
+def validatecache(filename, dict):
+    cached = createdict(filename)
+    if cached = dict:
+        return True
+    else:
+        return False
