8e40599cdecc713e559408b5a5a6c62b02508bb7
RhinosF1 <rhinosf1@gmail.com>
2020-08-14 22:26:00 +0100

    Revert "fix"
    This reverts commit 6540f465901dbc9e7fd46e2467af14a200cc951f.

--- MirahezeBots/plugins/wikimgnt.py
+++ MirahezeBots/plugins/wikimgnt.py
@@ -35,8 +35,11 @@ def setup(bot):
     elif bot.settings.wikimgnt.wiki_farm is False and bot.settings.wikimgnt.log_page is None:
         raise ConfigurationError("For single wikis, log_page must be defined")
     elif bot.settings.wikimgnt.datafile:
+        LOGGER.warn("got to datafile execution")
         bot.memory["wikimgnt"] = SopelMemory()
+        LOGGER.warn("executed step 1 correctly")
         bot.memory["wikimgnt"]["jdcache"] = jp.createdict(bot.settings.wikimgnt.datafile)
+        LOGGER.warn("executed step 2 correctly")
 
 
 def configure(config):
@@ -116,7 +119,7 @@ def logpage(bot, trigger):
             bot.say("Syntax: .log wiki message")
             return
     sender = trigger.nick
-    requestdata = [trigger.account, options[0]]
+    requestdata = [trigger.account, options[1]]
     if bot.settings.wikimgnt.wiki_farm is True:
         url = options[0] + bot.settings.wikimgnt.wiki_domain
         message = options[1]
