93ce584d72c7ad0fdebcc8684b5eb1796618cd6d
RhinosF1 <rhinosf1@gmail.com>
2020-08-17 10:34:06 +0100

    Create test_json.py

---
+++ tests/test_json.py
@@ -0,0 +1,5 @@
+from MirahezeBots.utils import jsonparser as jp
+
+DICT = {'#miraheze-cvt-private': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
+
+def test_create_dict():
