c1f3c32da22fdaf5c8306457fde4f0803537538c
RhinosF1 <rhinosf1@gmail.com>
2020-08-17 10:57:22 +0100

    test_json: fix and add 2 more

--- tests/test_json.py
+++ tests/test_json.py
@@ -3,8 +3,18 @@ import os
 
 
 DICT = {'#miraheze-cvt-private': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
-PATH = '../MirahezeBots/MirahezeBots/tests'
 
+ALTDICT = {'#miraheze-cvt': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
 
 def test_create_dict():
-    assert DICT == jp.createdict(os.path.join(PATH, 'test_json.json'))
+    assert DICT == jp.createdict('tests/test_json.json')
+
+ 
+def test_check_dict_true():
+    result = jp.validatecache('tests/test_json.json', DICT)
+    assert result == True
+    
+    
+def test_check_dict_false():
+    result = jp.validatecache('tests/test_json.json', ALTDICT)
+    assert result == False
