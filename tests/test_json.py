4408a57a0348f24ec28943b19bbde4df248f106b
RhinosF1 <rhinosf1@gmail.com>
2020-08-17 10:40:34 +0100

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,7 +1,10 @@
 from MirahezeBots.utils import jsonparser as jp
+import os
+
 
 DICT = {'#miraheze-cvt-private': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
+PATH = '../MirahezeBots/MirahezeBots/tests'
 
 
 def test_create_dict():
-    assert dict == jp.createdict('test_json.json')
+    assert dict == jp.createdict(os.path.join(PATH, 'test_json.json'))
