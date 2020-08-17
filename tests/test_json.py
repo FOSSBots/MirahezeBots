8ad2902e97133ce348f127b384d1fada03f35788
RhinosF1 <rhinosf1@gmail.com>
2020-08-17 11:00:14 +0100

    test_json: flake8

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,20 +1,20 @@
 from MirahezeBots.utils import jsonparser as jp
-import os
 
 
 DICT = {'#miraheze-cvt-private': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
 
-ALTDICT = {'#miraheze-cvt': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
+ALTDICT = {'#miraheze-cvt-fake': {'inherits-from': ['#miraheze-cvt']}, '#miraheze-cvt': {'chanops': ['NDKilla', 'Voidwalker', 'Reception123', 'The_Pionner', 'JohnLewis', 'Zppix']}}
+
 
 def test_create_dict():
     assert DICT == jp.createdict('tests/test_json.json')
 
- 
+
 def test_check_dict_true():
     result = jp.validatecache('tests/test_json.json', DICT)
-    assert result == True
-    
-    
+    assert result is True
+
+
 def test_check_dict_false():
     result = jp.validatecache('tests/test_json.json', ALTDICT)
-    assert result == False
+    assert result is False
