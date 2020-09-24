8c80fc391b8c61569eff677b1f9df10e1f6bdbc6
MacFan4000 <paulfkeffer@gmail.com>
2020-09-24 14:26:11 -0400

    Update phab.py

--- MirahezeBots/plugins/phab.py
+++ MirahezeBots/plugins/phab.py
@@ -6,7 +6,7 @@ from sopel.config.types import StaticSection, ValidatedAttribute, ListAttribute
 from json import JSONDecodeError
 from sopel.tools import get_logger, SopelMemory
 from sopel.config import ConfigurationError
-from MirahezeBots.utils import jsonparser as jp
+import MirahezeBots-jsonparser as jp
 from urllib.parse import urlparse
 LOGGER = get_logger('phabricator')
 
