af768051d9865933d3cdd7b9b402adf4882a6866
RhinosF1 <rhinosf1@gmail.com>
2020-10-23 19:03:23 +0100

    phab: make highpri interval use base config / drop constant

--- MirahezeBots/plugins/phab.py
+++ MirahezeBots/plugins/phab.py
@@ -5,11 +5,6 @@ from sopel.module import commands, example, interval, rule
 from MirahezeBots.utils import phabsearch
 
 
-def setup(bot):
-    global PHAB_SETTINGS
-    PHAB_SETTINGS = bot.memory["settings"]["phab"]
-
-
 HIGHPRIO_TASKS_NOTIFICATION_INTERVAL = 7 * 24 * 60 * 60  # every week
 
 
@@ -21,7 +16,7 @@ def phabtask(bot, trigger):
             task_id = trigger.group(2).split('T')[1]
         else:
             task_id = trigger.group(2)
-        bot.say(phabsearch.searchphab(PHAB_SETTINGS[trigger.sender], task=task_id), trigger.sender)
+        bot.say(phabsearch.searchphab(bot.memory["settings"]["phab"][trigger.sender], task=task_id), trigger.sender)
     except AttributeError:
         bot.say('Syntax: .task (task ID with or without T)', trigger.sender)
 
@@ -30,17 +25,17 @@ def phabtask(bot, trigger):
 def phabtask2(bot, trigger):
     """Get a Miraheze phabricator link to a the task number you provide."""
     task_id = (trigger.match.group(0)).split('T')[1]
-    bot.say(phabsearch.searchphab(PHAB_SETTINGS[trigger.sender], task=task_id), trigger.sender)
+    bot.say(phabsearch.searchphab(bot.memory["settings"]["phab"][trigger.sender], task=task_id), trigger.sender)
 
 
 @interval(HIGHPRIO_TASKS_NOTIFICATION_INTERVAL)
 def high_priority_tasks_notification(bot):
     if bot.settings.phabricator.highpri_notify is True:
         """Send high priority tasks notifications."""
-        bot.say(phabsearch.gethighpri(PHAB_SETTINGS["fixme"]), "#fixme")
+        bot.say(phabsearch.gethighpri(bot.memory["settings"]["phab"]["base"]), bot.memory["settings"]["phab"]["base"]["phab-highpri-channel"])
 
 
 @commands('highpri')
 @example('.highpri')
 def forcehighpri(bot, trigger):
-    bot.say(phabsearch.gethighpri(PHAB_SETTINGS[trigger.sender], limit=False), trigger.sender)  # will need changing
+    bot.say(phabsearch.gethighpri(bot.memory["settings"]["phab"][trigger.sender], limit=False), trigger.sender)  # will need changing
