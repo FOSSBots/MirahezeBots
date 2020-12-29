b4744cc8e800bbbc2d21ca98a522efa5778ef6f2
RhinosF1 <rhinosf1@gmail.com>
2020-12-29 19:49:16 +0000

    syntax

--- MirahezeBots/utils/phabapi.py
+++ MirahezeBots/utils/phabapi.py
@@ -21,59 +21,59 @@ def gettaskinfo(host, apikey, task=1):
         return "An unknown error occured."
     params = {
         'api.token': apikey,
-         'constraints[phids][0]': result.get("fields").get("ownerPHID")
+        'constraints[phids][0]': result.get("fields").get("ownerPHID")
     }
     response2 = requests.post(
-            url='{0}/user.search'.format(host),
-            data=params)
+        url='{0}/user.search'.format(host),
+        data=params)
     try:
         response2 = response2.json()
     except JSONDecodeError as e:
-        raise JSONDecodeError("Encountered {0} on {1}".format(e, response2.text)
-    params2={
-       'api.token': apikey,
-       'constraints[phids][0]': result.get("fields").get("authorPHID")
+        raise JSONDecodeError("Encountered {0} on {1}".format(e, response2.text))
+    params2 = {
+        'api.token': apikey,
+        'constraints[phids][0]': result.get("fields").get("authorPHID")
     }
-    response3=requests.post(
+    response3 = requests.post(
         url='{0}/user.search'.format(host),
         data=params2)
-    response3=response3.json()
+    response3 = response3.json()
     if result.get("fields").get("ownerPHID") is None:
-         owner=None
+        owner = None
     else:
-        owner=response2.get("result").get("data")[0].get("fields").get("username")
-    author=response3.get("result").get("data")[0].get("fields").get("username")
-    priority=result.get("fields").get("priority").get("name")
-    status=result.get("fields").get("status").get("name")
-    output='{0}/T{1} - '.format("https://" + str(urlparse(host).netloc), str(result["id"]))
-    output='{0}{2}{1}{2}, '.format(output, str(result.get('fields').get('name')), BOLD)
-    output=output + 'authored by {1}{0}{1}, '.format(author, BOLD)
-    output=output + 'assigned to {1}{0}{1}, '.format(owner, BOLD)
-    output=output + 'Priority: {1}{0}{1}, '.format(priority, BOLD)
-    output=output + 'Status: {1}{0}{1}'.format(status, BOLD)
+        owner = response2.get("result").get("data")[0].get("fields").get("username")
+    author = response3.get("result").get("data")[0].get("fields").get("username")
+    priority = result.get("fields").get("priority").get("name")
+    status = result.get("fields").get("status").get("name")
+    output = '{0}/T{1} - '.format("https://" + str(urlparse(host).netloc), str(result["id"]))
+    output = '{0}{2}{1}{2}, '.format(output, str(result.get('fields').get('name')), BOLD)
+    output = output + 'authored by {1}{0}{1}, '.format(author, BOLD)
+    output = output + 'assigned to {1}{0}{1}, '.format(owner, BOLD)
+    output = output + 'Priority: {1}{0}{1}, '.format(priority, BOLD)
+    output = output + 'Status: {1}{0}{1}'.format(status, BOLD)
     return output
 
 
-  def dophabsearch(limit=True, host, apikey):
+def dophabsearch(host, apikey, limit=True):
     """Performs a maniphest search."""
-    data={
+    data = {
         'api.token': apikey,
         'queryKey': querykey,
     }
-    response=requests.post(
+    response = requests.post(
         url='{0}/maniphest.search'.format(host),
         data=data)
-    response=response.json()
-    result=response.get("result")
+    response = response.json()
+    result = response.get("result")
     try:
-        data=result.get("data")
+        data = result.get("data")
     except Exception:
         return None
-    x=0
+    x = 0
     while x < len(data):
-        currdata=data[x]
+        currdata = data[x]
         if x > 5 and limit:
-             return  # fix
+            return  # fix
         else:
             searchphab(bot=bot, channel=channel, task=currdata.get("id"))
-            x=x + 1
+            x = x + 1
