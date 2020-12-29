0fa39ed5e8d9cabdab4bc1cab83b6e6ae3e5e94c
RhinosF1 <rhinosf1@gmail.com>
2020-12-29 22:22:46 +0000

    Update goofy.py

--- MirahezeBots/plugins/goofy.py
+++ MirahezeBots/plugins/goofy.py
@@ -6,7 +6,7 @@ from sopel.module import commands, example
 @example('.coffee MirahezeBot')
 @commands('coffee')
 def coffee(bot, trigger):
-    """Makes me give the specified nick a coffee."""
+    """Make me give the specified nick a coffee."""
     if trigger.group(2) is None:
         bot.reply("To whom should I give this cup of coffee?")
     else:
@@ -16,7 +16,7 @@ def coffee(bot, trigger):
 @example('.hug MirahezeBot')
 @commands('hug')
 def hug(bot, trigger):
-    """Makes me give the specified nick a hug."""
+    """Make me give the specified nick a hug."""
     if trigger.group(2) is None:
         bot.reply("To whom should I give this hug?")
     else:
@@ -26,7 +26,7 @@ def hug(bot, trigger):
 @example('.burger MirahezeBot')
 @commands('burger')
 def burger(bot, trigger):
-    """Makes me give the specified nick a burger."""
+    """Make me give the specified nick a burger."""
     if trigger.group(2) is None:
         bot.reply("To whom should I give this cheeseburger?")
     else:
@@ -36,7 +36,7 @@ def burger(bot, trigger):
 @example('.present MirahezeBot')
 @commands('present')
 def present(bot, trigger):
-    """Makes me give the specified nick a present."""
+    """Make me give the specified nick a present."""
     if trigger.group(2) is None:
         bot.reply("To whom should I give this present?")
     else:
@@ -46,7 +46,7 @@ def present(bot, trigger):
 @example('.hotchoc MirahezeBot')
 @commands('hotchoc', 'hotchocolate')
 def hotchoc(bot, trigger):
-    """Makes me give the specified nick a hot chocolate."""
+    """Make me give the specified nick a hot chocolate."""
     if trigger.group(2) is None:
         bot.reply("To whom should I give this hot chocolate?")
     else:
