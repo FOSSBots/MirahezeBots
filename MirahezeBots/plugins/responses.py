c9e319f1d86a1d8081a0fcff345c38a13de26bb6
RhinosF1 <rhinosf1@gmail.com>
2021-01-20 12:14:59 +0000

    Update responses.py

--- MirahezeBots/plugins/responses.py
+++ MirahezeBots/plugins/responses.py
@@ -9,62 +9,61 @@ from sopel.module import commands, example, rate, require_account
 class ResponsesSection(StaticSection):
     """Create configuration for Sopel."""
 
-    support_channel = ValidatedAttribute('support_channel', str)
+    support_channel = ValidatedAttribute("support_channel", str)
 
 
 def setup(bot):
     """Set up the config section."""
-    bot.config.define_section('responses', ResponsesSection)
+    bot.config.define_section("responses", ResponsesSection)
 
 
 def configure(config):
     """Set up the configuration options."""
-    config.define_section('responses', ResponsesSection, validate=False)
-    config.responses.configure_setting('support_channel', 'Specify a support IRC channel (leave blank for none).')
+    config.define_section("responses", ResponsesSection, validate=False)
+    config.responses.configure_setting("support_channel", "Specify a support IRC channel (leave blank for none).")
 
 
-@commands('addchannel')
-@example('.addchannel (insert which)')
+@commands("addchannel")
+@example(".addchannel (insert which)")
 @rate(user=120, channel=240, server=60)
 @require_account()
 def addchan(bot, trigger):
     """Reply to channel request message."""
-    admins = ' '.join(map(str, bot.config.core.admin_accounts))
+    admins = " ".join(map(str, bot.config.core.admin_accounts))
     if bot.config.responses.support_channel is not None:
         bot.say(f"Hey {admins}, {trigger.nick} would like to have me in their channel: {trigger.group(2)}", bot.config.responses.support_channel)
         if trigger.sender != bot.config.responses.support_channel:
             bot.reply("Request sent! Action upon the request should be taken shortly. Thank you for using {}!".format(bot.nick))
 
 
-@commands('gj', 'gw')
-@example('.gj (nick)')
+@commands("gj", "gw")
+@example(".gj (nick)")
 @rate(user=2, channel=1, server=0)
 def gj(bot, trigger):
     """Tell the user that they are doing good work."""
     bot.say(("You're doing good work, {}!").format(trigger.group(2)))
 
 
-@commands('cancelreminder')
-@example('.cancelreminder (insert reminder message here)')
+@commands("cancelreminder")
+@example(".cancelreminder (insert reminder message here)")
 @rate(user=2, channel=1, server=0)
 def cancel(bot, trigger):
     """Cancel reminder."""
-    admins = ' '.join(map(str, bot.config.core.admin_accounts))
-    bot.reply(('Pinging {} to cancel '
-               '{}\'s reminder.').format(admins, trigger.nick))
+    admins = " ".join(map(str, bot.config.core.admin_accounts))
+    bot.reply(f"Pinging {admins} to cancel {trigger.nick}'s reminder.")
 
 
-@commands('botversion', 'bv')
-@example('.botversion')
+@commands("botversion", "bv")
+@example(".botversion")
 @rate(user=2, channel=1, server=0)
 def botversion(bot, trigger):  # noqa: U100
     """List the current version of the bot."""
-    bot.say('The current version of this bot is {} ({})'.format(VERSION, SHORTVERSION))
+    bot.say(f"The current version of this bot is {VERSION} ({SHORTVERSION})")
 
 
-@commands('source', 'botsource')
-@example('.source')
+@commands("source", "botsource")
+@example(".source")
 @rate(user=2, channel=1, server=0)
 def githubsource(bot, trigger):  # noqa: U100
     """Give the link to MirahezeBot's Github."""
-    bot.reply('My code can be found here: https://github.com/MirahezeBots/MirahezeBots')
+    bot.reply("My code can be found here: https://github.com/MirahezeBots/MirahezeBots")
