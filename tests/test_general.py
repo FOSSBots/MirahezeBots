d30d5b4b6924de3302f1852f3d358f12fd850f46
RhinosF1 <rhinosf1@gmail.com>
2021-01-20 19:47:59 +0000

    Update test_general.py

--- tests/test_general.py
+++ tests/test_general.py
@@ -34,10 +34,10 @@ def test_db_schema_is_same():
 def test_no_get_on_lists():
     """Checks for misuse of .get() on lists."""
     reg = r'get\([0-9]'
-    for top, dirs, files in os.walk(PLUGINPATH):	
-        for filen in files:	
-            if not filen.endswith('.py'):	
-                continue	
+    for top, dirs, files in os.walk(PLUGINPATH):
+        for filen in files:
+            if not filen.endswith('.py'):
+                continue
             with open(os.path.join(PLUGINPATH, filen)) as python_source:		
                 src = python_source.read()
                 assert not re.search(reg, src)
