01f8e70728dd466d929c17d84e114b2d1ba1c801
RhinosF1 <rhinosf1@gmail.com>
2021-01-23 16:45:24 +0000

    Update status.py

--- MirahezeBots/plugins/status.py
+++ MirahezeBots/plugins/status.py
@@ -4,11 +4,12 @@ from MirahezeBots.utils import mwapihandler as mwapi
 
 from MirahezeBots_jsonparser import jsonparser as jp
 
+from requests import Session
+
 from sopel.config.types import StaticSection, ValidatedAttribute
 from sopel.module import commands, example, require_admin
 from sopel.tools import SopelMemory
 
-from requests import Session
 
 pages = ''
 
@@ -28,7 +29,6 @@ def setup(bot):
     bot.memory['status'] = SopelMemory()
     bot.memory['status']['jdcache'] = jp.createdict(bot.settings.status.datafile)
     bot.memory['status']['session'] = Session()
-
 
 
 def configure(config):
