dcd5d3a3f3246e1f2d039cc39184c505ec23252e
RhinosF1 <rhinosf1@gmail.com>
2021-02-24 13:10:44 +0000

    Update phabapi.py

--- MirahezeBots/utils/phabapi.py
+++ MirahezeBots/utils/phabapi.py
@@ -3,9 +3,9 @@
 from json import JSONDecodeError
 from urllib.parse import urlparse
 
-from requests import Session
-
 from warnings import warn
+
+from requests import Session
 
 from requests_cache import install_cache, uninstall_cache
 
