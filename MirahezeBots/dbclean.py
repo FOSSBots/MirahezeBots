8852c21108b0b357a8da3c8e44eff1fb61ef49cd
MacFan4000 <paulfkeffer@gmail.com>
2021-06-23 13:52:18 -0400

    Update dbclean.py

--- MirahezeBots/dbclean.py
+++ MirahezeBots/dbclean.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def main():
     file = input("Full path to the deletion list: ")
-    with open(file, 'r') as f: # ensure the file is open and closed properly
+    with open(file, 'r') as f:  # ensure the file is open and closed properly
         users = f.readlines()
     database = input("Full path to database: ")
     with sqlite3.connect(database) as conn:
@@ -13,4 +13,6 @@ def main():
             curs.execute('DELETE FROM nicknames WHERE nick_id = ?', (user,))
             curs.execute('DELETE FROM nick_ids WHERE nick_id = ?', (user,))
         conn.commit()
+
+
 main()
