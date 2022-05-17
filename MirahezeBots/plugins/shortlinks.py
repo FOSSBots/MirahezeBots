8be5cacef157d6c3df4065cbcd5adb59707de3c4
RhinosF1 <rhinosf1@gmail.com>
2022-05-17 20:59:22 +0100

    shortlinks: quote url paths

--- MirahezeBots/plugins/shortlinks.py
+++ MirahezeBots/plugins/shortlinks.py
@@ -2,13 +2,15 @@
 from sopel.plugin import commands, example
 from sopel import bot, trigger
 
+from urllib.parse import quote
+
 
 @commands('github', 'gh')
 @example('.github user')
 def ghuser(instance: bot, message: trigger) -> None:
     """Expand a link to github."""
     try:
-        instance.say(f'https://github.com/{message.group(2)}')
+        instance.say(f'https://github.com/{quote(message.group(2))}')
     except TypeError:
         instance.say('Syntax: .github user', message.sender)
 
@@ -18,7 +20,7 @@ def ghuser(instance: bot, message: trigger) -> None:
 def redditu(instance: bot, message: trigger) -> None:
     """Expand a link to reddit/u."""
     try:
-        instance.say(f'https://reddit.com/u/{message.group(2)}')
+        instance.say(f'https://reddit.com/u/{quote(message.group(2))}')
     except TypeError:
         instance.say('Syntax: .redditu example', message.sender)
 
@@ -28,7 +30,7 @@ def redditu(instance: bot, message: trigger) -> None:
 def redditr(instance: bot, message: trigger) -> None:
     """Expand a link to reddit/r."""
     try:
-        instance.say(f'https://reddit.com/r/{message.group(2)}')
+        instance.say(f'https://reddit.com/r/{quote(message.group(2))}')
     except TypeError:
         instance.say('Syntax: .subred example', message.sender)
 
@@ -39,7 +41,7 @@ def wmca(instance: bot, message: trigger) -> None:
     """Expand a link to Wikimedia CentralAuth."""
     try:
         instance.say(
-            f'https://meta.wikimedia.org/wiki/Special:CentralAuth/{message.group(2).replace(" ", "_")}',
+            f'https://meta.wikimedia.org/wiki/Special:CentralAuth/{quote(message.group(2).replace(" ", "_"))}',
             )
     except AttributeError:
         instance.say('Syntax: .wmca example', message.sender)
@@ -51,7 +53,7 @@ def mhca(instance: bot, message: trigger) -> None:
     """Expand a link to Miraheze Central Auth."""
     try:
         instance.say(
-            f'https://meta.miraheze.org/wiki/Special:CentralAuth/{message.group(2).replace(" ", "_")}',
+            f'https://meta.miraheze.org/wiki/Special:CentralAuth/{quote(message.group(2).replace(" ", "_"))}',
             )
     except AttributeError:
         instance.say('Syntax: .mhca example', message.sender)
@@ -62,7 +64,7 @@ def mhca(instance: bot, message: trigger) -> None:
 def twlink(instance: bot, message: trigger) -> None:
     """Expand a link to Twitter."""
     try:
-        instance.say(f'https://twitter.com/{message.group(2)}')
+        instance.say(f'https://twitter.com/{quote(message.group(2))}')
     except TypeError:
         instance.say('Syntax: .tw user', message.sender)
 
@@ -75,11 +77,11 @@ def mhwiki(instance: bot, message: trigger) -> None:
         options = message.group(2).split(' ', 1)
         if len(options) == 1:
             instance.say(
-                f'https://meta.miraheze.org/wiki/{options[0].replace(" ", "_")}',
+                f'https://meta.miraheze.org/wiki/{quote(options[0].replace(" ", "_"))}',
                 )
         elif len(options) == 2:
             wiki = options[0]
-            page = options[1].replace(' ', '_')
+            page = quote(options[1].replace(' ', '_'))
             instance.say(f'https://{wiki}.miraheze.org/wiki/{page}')
     except AttributeError:
         instance.say('Syntax: .mh wiki page', message.sender)
